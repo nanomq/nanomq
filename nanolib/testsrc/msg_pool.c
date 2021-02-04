d44a11b5226444e345600cde404027cf5368c3b0
wanghaemq <wangwei@emqx.io>
2021-02-04 19:18:58 +0800

    * FIX [nanolib] add tests in condition of concurrent

--- nanolib/testsrc/msg_pool.c
+++ nanolib/testsrc/msg_pool.c
@@ -1,64 +1,131 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <pthread.h>
+
 #include "../include/msg_pool.h"
 #include "../include/dbg.h"
 
+#define NUM_THREADS 5
+
+typedef struct thread_args {
+	int num;
+	nng_msg * msgs[200];
+}targs;
+
+nnl_msg_pool * mp;
+
+uint8_t test_one_get_one_put()
+{
+	uint8_t rv = 0;
+	nng_msg * msg;
+	rv |= nnl_msg_get(mp, &msg);
+	rv |= nnl_msg_put(mp, &msg);
+	return rv;
+}
+
+uint8_t test_multi_get(int n, nng_msg ** msgs)
+{
+	nng_msg * msg;
+	uint8_t rv = 0;
+	for(int i=0; i<n; i++){
+		rv |= nnl_msg_get(mp, &msgs[i]);
+		if(rv) log("rv: [%d] address [%p]", rv, msgs[i]);
+	}
+	return rv;
+}
+
+uint8_t test_multi_put(int n, nng_msg ** msgs)
+{
+	nng_msg * msg;
+	uint8_t rv = 0;
+	for(int i=0; i<n; i++){
+		if (msgs[i]) {
+			rv |= nnl_msg_put(mp, &msgs[i]);
+		} else {
+			rv = 1;
+		}
+		if(rv) log("rv: [%d] address [%p]", rv, msgs[i]);
+	}
+	return rv;
+}
+
+nng_msg * msgs[1000];
+
+void * thread_test_multi_get(void * n)
+{
+	uint8_t rv = test_multi_get(*(int *)n, msgs);
+	pthread_exit((void *)rv);
+}
+
+void * thread_test_multi_put(void * n)
+{
+	uint8_t rv = test_multi_put(*(int *)n, msgs);
+	pthread_exit((void *)rv);
+}
+
+uint8_t test_one_concurrent()
+{
+	uint8_t   rv;
+	pthread_t threads[2];
+	void * status;
+	int num[1] = {1000};
+
+	rv |= pthread_create(&threads[0], NULL, thread_test_multi_get, (void *)&num[0]);
+	rv |= pthread_create(&threads[1], NULL, thread_test_multi_put, (void *)&num[0]);
+
+	if (!rv) {
+		rv |= pthread_join(threads[0], &status);
+		log("rv about T0 [%d]", rv);
+		rv |= pthread_join(threads[1], &status);
+		log("rv about T1 [%d]", rv);
+	}
+
+	return rv;
+}
+
+void * thread_test_one_get_one_put(void * t)
+{
+	uint8_t rv = 0;
+	targs * ta = t;
+	for (int i=0; i<ta->num; i++)
+		rv += test_one_get_one_put();
+	free(ta);
+	pthread_exit((void *)&rv);
+}
+
+uint8_t test_two_concurrent()
+{
+	uint8_t   rv = 0;
+	pthread_t threads[200];
+	void * status;
+	int num = 200;
+
+	for (int i=0; i<200; i++) {
+		targs * t = malloc(sizeof(targs));
+		rv |= pthread_create(&threads[i], NULL, thread_test_one_get_one_put, (void *)t);
+		if (rv) {
+			log("ERROR in thread create");
+			break;
+		}
+	}
+
+	if (!rv) {
+		for (int i=0; i<200; i++) {
+			rv |= pthread_join(threads[0], &status);
+			log("rv about Tx [%d]", rv);
+		}
+	}
+
+	return rv;
+}
+
 char * test_msg_pool()
 {
-	nnl_msg_pool * mp;
-	nng_msg * msg, * msgs[65];
-	uint8_t rv = 0;
+	uint8_t      rv = 0;
 	log("create msg pool?");
 	CHECK(nnl_msg_pool_create(&mp) == 0);
-	log("get?");
-	CHECK(nnl_msg_get(mp, &msg) == 0);
-	log("used 1 ?");
-	CHECK(nnl_msg_pool_used(mp) == 1);
-	log("put?");
-	CHECK(nnl_msg_put(mp, &msg) == 0);
-	log("used 0 ?");
-	CHECK(nnl_msg_pool_used(mp) == 0);
-	log("resize 32 ?");
-	CHECK(nnl_msg_pool_resize(mp, 32) == 0);
-	log("capacity 32?");
-	CHECK(nnl_msg_pool_capacity(mp) == 32);
-	log("empty?");
-	CHECK(nnl_msg_pool_empty(mp));
-	log("full?");
-	CHECK(nnl_msg_pool_full(mp));
-	log("boundary test1 for get msg ?");
-	rv = 0;
-	for(int i=0; i<65; i++){
-		rv |= nnl_msg_get(mp, &msgs[i]);
-		log("rv: [%d] address [%p]", rv, msgs[i]);
-	}
-	rv = 0;
-	log("boundary test1 for put msg?");
-	for(int i=0; i<65; i++){
-		rv |= nnl_msg_put(mp, &msgs[i]);
-		log("rv: [%d] address [%p]", rv, msgs[i]);
-	}
-	log("resize 64 ?");
-	CHECK(nnl_msg_pool_resize(mp, 64) == 0);
-	log("capacity 64 ?");
-	CHECK(nnl_msg_pool_capacity(mp) == 64);
-	log("boundary test2 for get msg ?");
-	rv = 0;
-	for(int i=0; i<32; i++){
-		rv |= nnl_msg_get(mp, &msgs[i]);
-		log("rv: [%d] address [%p]", rv, msgs[i]);
-	}
-	log("resize 32 ?");
-	CHECK(nnl_msg_pool_resize(mp, 32) == 0);
-	log("capacity 32 ?");
-	CHECK(nnl_msg_pool_capacity(mp) == 32);
-	rv = 0;
-	log("boundary test2 for put msg?");
-	for(int i=0; i<32; i++){
-		rv |= nnl_msg_put(mp, &msgs[i]);
-		log("rv: [%d] address [%p]", rv, msgs[i]);
-	}
+	CHECK(test_two_concurrent() == 0);
 	log("delete msg pool.");
 	nnl_msg_pool_delete(mp);
 	return NULL;
