7ab0a38fa8df4e4d0954b8985f0c61931538152a
JaylinYu <64823539+JaylinYu@users.noreply.github.com>
2021-05-17 13:59:19 +0800

    Keep NanoMQ's MQ feature

--- nanomq/apps.c
+++ nanomq/apps.c
@@ -14,9 +14,9 @@
 #include <stdlib.h>
 
 #if defined(MQ)
-// NANOMQ_APP(mq, mqcreate_debug, mqsend_debug, mqreceive_debug);
+    NANOMQ_APP(mq, mqcreate_debug, mqsend_debug, mqreceive_debug);
 #endif
-NANOMQ_APP(broker, broker_dflt, broker_start, NULL);
+    NANOMQ_APP(broker, broker_dflt, broker_start, NULL);
 
 #if defined(NANO_DEBUG)
 
@@ -24,7 +24,7 @@ NANOMQ_APP(broker, broker_dflt, broker_start, NULL);
 
 const struct nanomq_app *edge_apps[] = {
 #if defined(MQ)
-	// &nanomq_app_mq,
+        &nanomq_app_mq,
 #endif
 	&nanomq_app_broker,
 #if defined(NANO_DEBUG)
