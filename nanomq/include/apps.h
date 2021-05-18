e00823c5f82c2c76eb4c31f4c65ae97d60915ee4
Hongwei-W <hongwei_charles_wang@outlook.com>
2021-05-18 15:47:29 +0800

    * FIX [broker/nanomq] format code

--- nanomq/include/apps.h
+++ nanomq/include/apps.h
@@ -9,13 +9,13 @@ struct nanomq_app {
 	int (*restart)(int argc, char **argv);
 };
 
-#define NANOMQ_APP(_name, _dflt, _start, _stop, _restart) 		\
-const struct nanomq_app nanomq_app_##_name = {		\
-	.name = #_name,					\
-	.dflt = _dflt,					\
-	.start = _start,				\
-	.stop = _stop,					\
-	.restart = _restart,			\
+#define NANOMQ_APP(_name, _dflt, _start, _stop, _restart) \
+const struct nanomq_app nanomq_app_##_name = {            \
+	.name = #_name,                                       \
+	.dflt = _dflt,                                        \
+	.start = _start,                                      \
+	.stop = _stop,                                        \
+	.restart = _restart,                                  \
 }
 
 extern const struct nanomq_app *edge_apps[];
