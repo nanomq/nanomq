af07df22096238d85cbe4c5905fc99d306f70987
JaylinYu <jaylin@emqx.io>
2021-07-09 11:40:09 +0800

    * MDF [broker] rename emq_worker to nano_worker (branding issue)

--- nanomq/include/unsub_handler.h
+++ nanomq/include/unsub_handler.h
@@ -6,9 +6,9 @@
 #include "broker.h"
 #include "packet.h"
 
-uint8_t decode_unsub_message(emq_work *);
-uint8_t encode_unsuback_message(nng_msg *, emq_work *);
-uint8_t unsub_ctx_handle(emq_work *);
+uint8_t decode_unsub_message(nano_work *);
+uint8_t encode_unsuback_message(nng_msg *, nano_work *);
+uint8_t unsub_ctx_handle(nano_work *);
 void    destroy_unsub_ctx(packet_unsubscribe *);
 
 #endif // MQTT_UNSUBSCRIBE_HANDLE_H
