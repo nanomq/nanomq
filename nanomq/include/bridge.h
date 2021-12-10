098c1f9d5a24dae5b7e1bb4d84791d8ef1823499
JaylinYu <jaylin@emqx.io>
2021-12-10 15:49:34 +0800

    * NEW [bridge] add bridging func.

---
+++ nanomq/include/bridge.h
@@ -0,0 +1,13 @@
+#ifndef NANOMQ_BRIDGE_H
+#define NANOMQ_BRIDGE_H
+
+#include <nng/nng.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int inner_client(nng_socket *sock, const char *url);
+extern int bridge_client(nng_socket *sock, const char *url, uint16_t nwork);
+extern int client_publish(nng_socket sock, const char *topic, uint8_t *payload,
+    uint32_t len, bool dup, uint8_t qos, bool retain);
+
+#endif // NANOMQ_BRIDGE_H
