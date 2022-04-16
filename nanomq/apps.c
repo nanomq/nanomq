52f59057eaa39dc4d2aa4a195f3e7f592ee2be8f
Jaylin <jaylin@emqx.io>
2022-04-16 11:15:21 +0800

    * NEW [app] add entry point of nngproxy in apps.c

--- nanomq/apps.c
+++ nanomq/apps.c
@@ -1,5 +1,5 @@
 //
-// Copyright 2020 NanoMQ Team, Inc. <jaylin@emqx.io>
+// Copyright 2022 NanoMQ Team, Inc. <jaylin@emqx.io>
 //
 // This software is supplied under the terms of the MIT License, a
 // copy of which should be located in the distribution where this
@@ -13,6 +13,7 @@
 #include "include/mq.h"
 #include "include/nngcat.h"
 #include "include/bench.h"
+#include "include/nng_proxy.h"
 
 #include <stdlib.h>
 
@@ -24,6 +25,12 @@ NANOMQ_APP(broker, broker_dflt, broker_start, broker_stop, broker_restart);
 NANOMQ_APP(pub, pub_dflt, pub_start, NULL, client_stop);
 NANOMQ_APP(sub, sub_dflt, sub_start, NULL, client_stop);
 NANOMQ_APP(conn, conn_dflt, conn_start, NULL, client_stop);
+#endif
+
+#if defined(SUPP_NNG_PROXY)
+NANOMQ_APP(nngproxy, nng_proxy_start, NULL, NULL, NULL);
+NANOMQ_APP(nngpub0, nng_pub0_dflt, nng_pub0_start, NULL, nng_client0_stop);
+NANOMQ_APP(nngsub0, nng_sub0_dflt, nng_sub0_start, NULL, nng_client0_stop);
 #endif
 #if defined(SUPP_BENCH)
 NANOMQ_APP(bench, bench_dflt, bench_start, NULL, NULL);
@@ -43,10 +50,15 @@ const struct nanomq_app *edge_apps[] = {
 	&nanomq_app_sub,
 	&nanomq_app_conn,
 #endif
+#if defined(SUPP_NNG_PROXY)
+	&nanomq_app_nngproxy,
+	&nanomq_app_nngpub0,
+	&nanomq_app_nngsub0,
+	&nanomq_app_nngcat,
+#endif
 #if defined(SUPP_BENCH)
 	&nanomq_app_bench,
 #endif
-	&nanomq_app_nngcat,
 #if defined(NANO_DEBUG)
 //&
 #endif
