0ec97f81c1799938a73c72c93e31623498557b68
alvin1221 <alvin@emqx.io>
2022-05-12 11:01:05 +0800

    * NEW [conf] add data structure`conf_auth_http`

--- nanolib/include/conf.h
+++ nanolib/include/conf.h
@@ -18,6 +18,7 @@
 #define CONF_BRIDGE_PATH_NAME "/etc/nanomq_bridge.conf"
 #define CONF_GATEWAY_PATH_NAME "/etc/nanomq_gateway.conf"
 #define CONF_WEB_HOOK_PATH_NAME "/etc/nanomq_web_hook.conf"
+#define CONF_AUTH_HTTP_PATH_NAME "/etc/nanomq_auth_http.conf"
 
 #define CONF_TCP_URL_DEFAULT "nmq-tcp://0.0.0.0:1883"
 #define CONF_TLS_URL_DEFAULT "tls+nmq-tcp://0.0.0.0:8883"
@@ -61,6 +62,58 @@ struct conf_tls {
 };
 
 typedef struct conf_tls conf_tls;
+
+struct conf_http_header {
+	char *key;
+	char *value;
+};
+
+typedef struct conf_http_header conf_http_header;
+
+typedef enum {
+	ACCESS,
+	USERNAME,
+	CLIENTID,
+	IPADDRESS,
+	PROTOCOL,
+	PASSWORD,
+	SOCKPORT,    // sockport of server accepted
+	COMMON_NAME, // common name of client TLS cert
+	SUBJECT,     // subject of client TLS cert
+	TOPIC,
+} http_param_type;
+
+struct conf_http_param {
+	char *          name;
+	http_param_type type;
+};
+
+typedef struct conf_http_param conf_http_param;
+
+struct conf_auth_http_req {
+	char *url;
+	char *method;
+	size_t header_count;
+	conf_http_header **headers;
+	size_t param_count;
+	conf_http_param **params;
+};
+
+typedef struct conf_auth_http_req conf_auth_http_req;
+
+struct conf_auth_http {
+	bool               enable;
+	conf_auth_http_req auth_req;
+	conf_auth_http_req super_req;
+	conf_auth_http_req acl_req;
+	uint64_t           timeout;         // seconds
+	uint64_t           connect_timeout; // seconds
+	size_t             pool_size;
+	// TODO not support yet
+	conf_tls tls;
+};
+
+typedef struct conf_auth_http conf_auth_http;
 
 struct conf_jwt {
 	char *iss;
@@ -167,20 +220,13 @@ struct conf_web_hook_rule {
 
 typedef struct conf_web_hook_rule conf_web_hook_rule;
 
-struct conf_web_hook_header {
-	char *key;
-	char *value;
-};
-
-typedef struct conf_web_hook_header conf_web_hook_header;
-
 struct conf_web_hook {
 	bool   enable;
 	char * url;
 	size_t pool_size;
 	hook_payload_type encode_payload;
 	size_t header_count;
-	conf_web_hook_header **headers;
+	conf_http_header **headers;
 
 	uint16_t            rule_count;
 	conf_web_hook_rule **rules;
@@ -194,8 +240,9 @@ typedef struct conf_web_hook  conf_web_hook;
 struct conf {
 	char *   conf_file;
 	char *   bridge_file;
-	char * 	 web_hook_file;
+	char *   web_hook_file;
 	char *   auth_file;
+	char *   auth_http_file;
 	char *   url; // "nmq-tcp://addr:port"
 	int      num_taskq_thread;
 	int      max_taskq_thread;
@@ -213,7 +260,8 @@ struct conf {
 	conf_bridge      bridge;
 	conf_web_hook    web_hook;
 
-	conf_auth auths;
+	conf_auth      auths;
+	conf_auth_http auth_http;
 };
 
 typedef struct conf conf;
