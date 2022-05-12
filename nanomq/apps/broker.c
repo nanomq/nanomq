0cfb4d6fa7ee654c7770a98fda7cdb2127b1fd7e
alvin1221 <alvin@emqx.io>
2022-05-12 11:01:05 +0800

    * NEW [opts] add cmd argument  `--auth_http <path> `

--- nanomq/apps/broker.c
+++ nanomq/apps/broker.c
@@ -56,6 +56,7 @@ enum options {
 	OPT_BRIDGEFILE,
 	OPT_WEBHOOKFILE,
 	OPT_AUTHFILE,
+	OPT_AUTH_HTTP_FILE,
 	OPT_PARALLEL,
 	OPT_DAEMON,
 	OPT_THREADS,
@@ -80,6 +81,7 @@ static nng_optspec cmd_opts[] = {
 	{ .o_name = "bridge", .o_val = OPT_BRIDGEFILE, .o_arg = true },
 	{ .o_name = "webhook", .o_val = OPT_WEBHOOKFILE, .o_arg = true },
 	{ .o_name = "auth", .o_val = OPT_AUTHFILE, .o_arg = true },
+	{ .o_name = "auth_http", .o_val = OPT_AUTH_HTTP_FILE, .o_arg = true },
 	{ .o_name = "daemon", .o_short = 'd', .o_val = OPT_DAEMON },
 	{ .o_name    = "tq_thread",
 	    .o_short = 't',
@@ -822,8 +824,9 @@ print_usage(void)
 	       "[--bridge <path>] \n                     "
 	       "[--webhook <path>] "
 		   "[--auth <path>] "
-	       "[-d, --daemon] "
-	       "[-t, --tq_thread <num>] \n                     "
+		   "[--auth_http <path>] "
+	       "[-d, --daemon] \n                     "
+	       "[-t, --tq_thread <num>] "
 	       "[-T, -max_tq_thread <num>] [-n, "
 	       "--parallel <num>]\n                     "
 	       "[-D, --qos_duration <num>] [--http] "
@@ -848,6 +851,9 @@ print_usage(void)
 	printf(
 	    "  --auth <path>              The path of a specified authorize "
 	    "configuration file \n");
+	printf("  --auth_http <path>         The path of a specified http "
+	       "authorize "
+	       "configuration file \n");
 	printf("  --http                     Enable http server (default: "
 	       "false)\n");
 	printf(
@@ -1015,6 +1021,10 @@ broker_parse_opts(int argc, char **argv, conf *config)
 			FREE_NONULL(config->auth_file);
 			config->auth_file = nng_strdup(arg);
 			break;
+		case OPT_AUTH_HTTP_FILE:
+			FREE_NONULL(config->auth_http_file);
+			config->auth_http_file = nng_strdup(arg);
+			break;
 		case OPT_PARALLEL:
 			config->parallel = atoi(arg);
 			break;
@@ -1128,6 +1138,7 @@ broker_start(int argc, char **argv)
 	conf_init(nanomq_conf);
 	conf_parser(nanomq_conf);
 	conf_bridge_parse(nanomq_conf);
+	conf_auth_http_parse(nanomq_conf);
 	read_env_conf(nanomq_conf);
 
 	if (!broker_parse_opts(argc, argv, nanomq_conf)) {
@@ -1143,6 +1154,9 @@ broker_start(int argc, char **argv)
 	}
 	if (nanomq_conf->web_hook_file) {
 		conf_web_hook_parse(nanomq_conf);
+	}
+	if (nanomq_conf->auth_http_file) {
+		conf_auth_http_parse(nanomq_conf);
 	}
 
 	nanomq_conf->url = nanomq_conf->url != NULL
