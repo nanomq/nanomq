9904f9866c5a46eed03ac41cda4ad16b61c28ab1
Jaylin <jaylin@emqx.io>
2022-05-28 16:38:06 +0800

    * MDF [app/mqtt_api] setting conf when init listener.

--- nanomq/include/mqtt_api.h
+++ nanomq/include/mqtt_api.h
@@ -5,7 +5,8 @@
 #include <nng/supplemental/tls/tls.h>
 #include <nng/supplemental/util/options.h>
 #include <nng/supplemental/util/platform.h>
+#include <nng/protocol/mqtt/mqtt.h>
 
-int nano_listen(nng_socket sid, const char *addr, nng_listener *lp, int flags);
+int nano_listen(nng_socket sid, const char *addr, nng_listener *lp, int flags, conf *conf);
 int init_listener_tls(nng_listener l, conf_tls *tls);
 #endif // MQTT_API_H
