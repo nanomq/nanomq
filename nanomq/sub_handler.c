28e7a46ed40239adc7e9affd45bb413ed63f6214
wanghaemq <wangwei@emqx.io>
2022-06-24 16:18:17 +0800

    * FIX [sub] Fix the missing in rebase.

--- nanomq/sub_handler.c
+++ nanomq/sub_handler.c
@@ -267,7 +267,7 @@ sub_ctx_handle(nano_work *work)
 		if (!topic_exist && topic_str) {
 			uint8_t ver = work->proto_ver;
 			dbtree_insert_client(
-			    work->db, topic_str, old_ctx, work->pid.id);
+			    work->db, topic_str, cli_ctx, work->pid.id);
 
 			dbhash_insert_topic(work->pid.id, topic_str);
 		}
