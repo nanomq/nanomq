d3fda6b435896caed3b5935d642976429837bd38
Jaylin <jaylin@emqx.io>
2022-06-28 17:10:19 +0800

    * NEW [webhook] complete the webhook_entry

--- nanomq/include/webhook_post.h
+++ nanomq/include/webhook_post.h
@@ -2,6 +2,7 @@
 #define WEBHOOK_POST_H
 
 #include "webhook_inproc.h"
+#include "broker.h"
 
 extern int webhook_msg_publish(nng_socket *sock, conf_web_hook *hook_conf,
     pub_packet_struct *pub_packet, const char *username,
@@ -12,6 +13,5 @@ extern int webhook_client_connack(nng_socket *sock, conf_web_hook *hook_conf,
 extern int webhook_client_disconnect(nng_socket *sock,
     conf_web_hook *hook_conf, uint8_t proto_ver, uint16_t keepalive,
     uint8_t reason, const char *username, const char *client_id);
-extern int webhook_entry(nng_socket *sock, conf_web_hook *hook_conf,
-    conn_param *cparam, uint8_t reason, uint8_t type);
+extern int webhook_entry(nano_work *work, uint8_t reason);
 #endif
