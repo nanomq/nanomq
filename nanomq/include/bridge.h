f226cdf900fe2e9f4bf8850631741c097f50166c
alvin1221 <alvin@emqx.io>
2022-06-28 14:54:30 +0800

    * MDF [bridge] adapt to multi bridge config.

--- nanomq/include/bridge.h
+++ nanomq/include/bridge.h
@@ -7,12 +7,13 @@
 #include <stdlib.h>
 
 typedef struct {
-	nng_socket * sock;
-	conf_bridge *config;
+	nng_socket *      sock;
+	conf_bridge_node *config;
 } bridge_param;
 
-extern bool     topic_filter(const char *origin, const char *input);
-extern int      bridge_client(nng_socket *sock, conf *config, conf_bridge *bridge_conf);
+extern bool topic_filter(const char *origin, const char *input);
+extern int  bridge_client(
+     nng_socket *sock, conf *config, conf_bridge_node *bridge_conf);
 extern nng_msg *bridge_publish_msg(const char *topic, uint8_t *payload,
     uint32_t len, bool dup, uint8_t qos, bool retain);
 
