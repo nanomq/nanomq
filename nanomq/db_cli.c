aa5e015e1ad07f6f17ef36d105322d3372c47686
lee-emqx <lihj@emqx.io>
2022-07-07 17:50:40 +0800

    * FIX [rule_engine] fixed compile error.

--- nanomq/db_cli.c
+++ nanomq/db_cli.c
@@ -1,10 +1,11 @@
 #if defined(SUPP_RULE_ENGINE)
 #include "db_cli.h"
+#include "nanomq.h"
 
 void* runNetwork(void *arg) {
 	fdb_error_t err = fdb_run_network();
 	 if (err) {
-		log_err("Run network error: %s", fdb_get_error(err));
+		debug_msg("Run network error: %s", fdb_get_error(err));
 		exit(1);
 	 }
 	return NULL;
@@ -14,14 +15,14 @@ void* runNetwork(void *arg) {
 FDBDatabase* openDatabase(pthread_t* netThread) {
 	 fdb_error_t err = fdb_setup_network();
 	 if (err) {
-		log_err("Setup network error: %s", fdb_get_error(err));
+		debug_msg("Setup network error: %s", fdb_get_error(err));
 		exit(1);
 	 }
 	pthread_create(netThread, NULL, runNetwork, NULL);
 	FDBDatabase* db;
 	err = fdb_create_database(NULL, &db);
 	if (err) {
-		log_err("create database error: %s", fdb_get_error(err));
+		debug_msg("create database error: %s", fdb_get_error(err));
 		exit(1);
 	}
 	return db;
