222ca6336ab1d603acfcbd5c2eb321cbe629ed26
alvin1221 <alvin@emqx.io>
2022-07-14 14:48:49 +0800

    * MDF [tools] remove all tool app from nanomq sub_directory.

--- nanomq/apps.c
+++ nanomq/apps.c
@@ -9,64 +9,12 @@
 
 #include "include/apps.h"
 #include "include/broker.h"
-#include "include/client.h"
-#include "include/mq.h"
-#include "include/nngcat.h"
-#include "include/bench.h"
-#include "include/nng_proxy.h"
-#include "include/zmq_gateway.h"
 
 #include <stdlib.h>
 
-#if defined(MQ)
-NANOMQ_APP(mq, mqcreate_debug, mqsend_debug, mqreceive_debug, NULL);
-#endif
 NANOMQ_APP(broker, broker_dflt, broker_start, broker_stop, broker_restart);
-#if defined(SUPP_CLIENT)
-NANOMQ_APP(pub, pub_start, pub_start, NULL, client_stop);
-NANOMQ_APP(sub, sub_start, sub_start, NULL, client_stop);
-NANOMQ_APP(conn, conn_start, conn_start, NULL, client_stop);
-#endif
-
-#if defined(SUPP_NNG_PROXY)
-NANOMQ_APP(nngproxy, nng_proxy_start, NULL, NULL, NULL);
-NANOMQ_APP(nngpub0, nng_pub0_dflt, nng_pub0_start, NULL, nng_client0_stop);
-NANOMQ_APP(nngsub0, nng_sub0_dflt, nng_sub0_start, NULL, nng_client0_stop);
-#endif
-
-#if defined(SUPP_ZMQ_GATEWAY)
-NANOMQ_APP(gateway, gateway_dflt, gateway_start, NULL, NULL);
-#endif
-#if defined(NANO_PLATFORM_WINDOWS) && defined(SUPP_BENCH)
-NANOMQ_APP(bench, bench_dflt, bench_start, NULL, NULL);
-#endif
-NANOMQ_APP(nngcat, nng_cat_dflt, NULL, NULL, NULL);
-#if defined(NANO_DEBUG)
-
-#endif
 
 const struct nanomq_app *edge_apps[] = {
-#if defined(MQ)
-	&nanomq_app_mq,
-#endif
 	&nanomq_app_broker,
-#if defined(SUPP_CLIENT)
-	&nanomq_app_pub,
-	&nanomq_app_sub,
-	&nanomq_app_conn,
-#endif
-#if defined(SUPP_NNG_PROXY)
-	&nanomq_app_nngproxy,
-	&nanomq_app_nngcat,
-#endif
-#if defined(SUPP_ZMQ_GATEWAY)
-	&nanomq_app_gateway,
-#endif
-#if defined(NANO_PLATFORM_WINDOWS) && defined(SUPP_BENCH)
-	&nanomq_app_bench,
-#endif
-#if defined(NANO_DEBUG)
-//&
-#endif
 	NULL,
 };
--- nanomq/include/nngcat.h
+++
@@ -1,6 +0,0 @@
-#ifndef NANOMQ_NNGCAT_H
-#define NANOMQ_NNGCAT_H
-
-int nng_cat_dflt(int ac, char **av);
-
-#endif // NANOMQ_NNGCAT_H
