35be491fc090f33e3561f557e7d335ed5cbb10c4
lee-emqx <lihj@emqx.io>
2022-08-05 16:14:56 +0800

    * FIX [workflow] Add return value for mqtt_test.

--- .github/scripts/test.py
+++ .github/scripts/test.py
@@ -1,25 +1,32 @@
-from cgi import print_arguments
 import subprocess
 import shlex
 import time
 
 from mqtt_test import mqtt_test
+from tls_test import tls_test
 
 
 if __name__=='__main__':
     nanomq = shlex.split("nanomq start --url tls+nmq-tcp://0.0.0.0:8883 --cacert /etc/certs/cacert.pem --cert /etc/certs/cert.pem --key /etc/certs/key.pem --qos_duration 1")
-    sub = shlex.split("mosquitto_sub -t abc")
-    pub = shlex.split("mosquitto_pub -t abc -m msg")
     nanomq = subprocess.Popen(nanomq, 
                            stdout=subprocess.PIPE,
                            universal_newlines=True)
 
-    time.sleep(1)
+    time.sleep(0.5)
 
-    print("mqtt test start")
-    mqtt_test()
-    print("mqtt test end")
+    print("mqtt v311 test start")
+    if False == mqtt_test():
+        nanomq.terminate()
+        print("mqtt v311 test failed")
+        raise AssertionError
+    print("mqtt v311 test end")
+
+    print("tls v311 test start")
+    if False == tls_test():
+        nanomq.terminate()
+        print("tls v311 test failed")
+        raise AssertionError
+    print("tls v311 test end")
 
 
     nanomq.terminate()
-
