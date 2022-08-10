d633aa1b89f089e2897a998ff9db8f79363aa485
lee-emqx <lihj@emqx.io>
2022-08-10 13:24:14 +0800

    * [rule_engine_repub] fixed turn off rule engine compile error.

--- nanomq/include/repub.h
+++ nanomq/include/repub.h
@@ -7,8 +7,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#if defined(SUPP_RULE_ENGINE)
 extern int nano_client(nng_socket *sock, repub_t *repub);
 extern int nano_client_publish(nng_socket *sock, const char *topic,
     uint8_t *payload, uint32_t len, uint8_t qos, property *props);
+#endif
 
 #endif // NANOMQ_REPUB_H
