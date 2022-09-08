9358b766e8b82afb21bbfda365a6468ef1758bba
lee-emqx <lihj@emqx.io>
2022-09-08 12:24:13 +0800

    * MDF [workflow] add many details for mqtt_test

--- .github/scripts/mqtt_test.py
+++ .github/scripts/mqtt_test.py
@@ -9,6 +9,8 @@ import os
 
 g_port = 1883
 g_addr = "127.0.0.1"
+g_sub = "mosquitto_sub"
+g_pub = "mosquitto_pub"
 
 g_url = " -h {addr} -p {port} ".format(addr = g_addr, port = g_port)
 
@@ -24,9 +26,13 @@ def cnt_message(cmd, n, pid, message):
             n.value += 1
 
 def test_clean_session():
-    clean_session_cmd = shlex.split("mosquitto_sub -t topic {} -q 1".format(g_url))
-    persist_session_cmd = shlex.split("mosquitto_sub -t topic {} -c -i id -q 1".format(g_url))
-    pub_cmd = shlex.split("mosquitto_pub -m message {} -t topic -q 1".format(g_url))
+    cs_cmd = g_sub + g_url + "-t topic -q 1"
+    ps_cmd = g_sub + g_url + "-t topic -c -i id -q 1"
+    p_cmd =  g_pub + g_url + "-t topic -m message -q 1"
+
+    clean_session_cmd = shlex.split(cs_cmd)
+    persist_session_cmd = shlex.split(ps_cmd)
+    pub_cmd = shlex.split(p_cmd)
     is_success = True
 
     # persistence session
@@ -57,6 +63,10 @@ def test_clean_session():
         print("clean session test passed!")
     else:
         is_success = False
+        print("Persistence sub client does not receive Session reservation message 'message'")
+        print(cs_cmd)
+        print(ps_cmd)
+        print(p_cmd)
         print("clean session test failed!")
 
     os.kill(pid.value, signal.SIGKILL)
@@ -70,9 +80,14 @@ def test_clean_session():
     return is_success
 
 def test_retain():
-    retain_pub_cmd = shlex.split("mosquitto_pub -m message {} -t topic -r".format(g_url))
-    clean_retain_pub_cmd = shlex.split("mosquitto_pub -n {} -t topic -r".format(g_url))
-    sub_cmd = shlex.split("mosquitto_sub -t topic {}".format(g_url))
+    rp_cmd = g_pub + g_url + "-m message -t topic -r"
+    crp_cmd = g_pub + g_url + "-n -t topic -r"
+    s_cmd =  g_sub + g_url + " -t topic"
+
+    retain_pub_cmd = shlex.split(rp_cmd)
+    clean_retain_pub_cmd = shlex.split(crp_cmd)
+    sub_cmd = shlex.split(s_cmd)
+
     process = subprocess.Popen(retain_pub_cmd, 
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
@@ -92,8 +107,11 @@ def test_retain():
 
     time.sleep(1)
     process.terminate()
-    if cnt.value != 1:
+    if cnt.value == 1:
         is_success = False
+        print(rp_cmd)
+        print(crp_cmd)
+        print(s_cmd)
         print("Retain test failed!")
     else:
         print("Retain test passed!")
@@ -102,10 +120,15 @@ def test_retain():
     return is_success
 
 def test_v4_v5():
-    sub_cmd_v4 = shlex.split("mosquitto_sub -t topic/v4/v5 {} -V 311".format(g_url))
-    sub_cmd_v5 = shlex.split("mosquitto_sub -t topic/v4/v5 {} -V 5".format(g_url))
-    pub_cmd_v4 = shlex.split("mosquitto_pub -m message  -t topic/v4/v5 {} -V 311".format(g_url))
-    pub_cmd_v5 = shlex.split("mosquitto_pub -m message  -t topic/v4/v5 {} -V 5".format(g_url))
+    s_cmd_v4 = g_sub + g_url + " -t topic/v5/v4 -V 311"
+    s_cmd_v5 = g_sub + g_url + " -t topic/v4/v5 -V 5"
+    p_cmd_v4 = g_pub + g_url + " -m message  -t topic/v4/v5 -V 311"
+    p_cmd_v5 = g_pub + g_url + " -m message  -t topic/v5/v4 -V 5"
+
+    sub_cmd_v4 = shlex.split(s_cmd_v4)
+    sub_cmd_v5 = shlex.split(s_cmd_v5)
+    pub_cmd_v4 = shlex.split(p_cmd_v4)
+    pub_cmd_v5 = shlex.split(p_cmd_v5)
 
     is_success = True
     cnt = Value('i', 0)
@@ -122,7 +145,9 @@ def test_v4_v5():
     pid = Value('i', 0)
 
     if cnt.value != 1:
-        print("V4/V5 test failed!")
+        print(s_cmd_v4)
+        print(p_cmd_v5)
+        print("V5 => v4 test failed!")
         return False
 
     process = Process(target=cnt_message, args=(sub_cmd_v5, cnt, pid, "message"))
@@ -137,15 +162,19 @@ def test_v4_v5():
     os.kill(pid.value, signal.SIGKILL)
 
     if cnt.value != 2:
-        print("V4/V5 test failed!")
+        print(s_cmd_v5)
+        print(p_cmd_v4)
+        print("V4 => V5 test failed!")
         return False
     print("V4/V5 test passed!")
     return True
             
 
 def test_will_topic():
-    pub_cmd = shlex.split("mosquitto_pub {} -t msg -d -l --will-topic will_topic --will-payload will_payload".format(g_url))
-    sub_cmd = shlex.split("mosquitto_sub -t will_topic {}".format(g_url))
+    p_cmd = g_pub + g_url + " -t msg -d -l --will-topic will_topic --will-payload will_payload"
+    s_cmd = g_sub + g_url + "-t will_topic"
+    pub_cmd = shlex.split(p_cmd)
+    sub_cmd = shlex.split(s_cmd)
 
     is_success = True
     cnt = Value('i', 0)
@@ -168,6 +197,9 @@ def test_will_topic():
         times += 1
         if times == 5:
             is_success = False
+            print("Sub client does not receive will message 'will_payload'")
+            print(s_cmd)
+            print(p_cmd)
             print("Will topic test failed!")
             break
     process.terminate()
@@ -175,9 +207,4 @@ def test_will_topic():
     return is_success
 
 def mqtt_test():
-    # TODO why will topic test can pass on local, but fail on github
-    # ret1 = test_will_topic()
-    ret2 = test_v4_v5()
-    ret3 = test_clean_session()
-    ret4 = test_retain()
-    return ret2 and ret3 and ret4
+    return test_will_topic() and test_v4_v5() and test_clean_session() and test_retain()
