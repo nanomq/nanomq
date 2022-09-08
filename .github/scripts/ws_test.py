e65c76a80f2b4cdf23220f69f63b9897d59a3dea
lee-emqx <lihj@emqx.io>
2022-09-08 10:10:55 +0800

    * FIX [workflow] fixed ws_test instability.

--- .github/scripts/ws_test.py
+++ .github/scripts/ws_test.py
@@ -1,27 +1,41 @@
 #!/usr/local/bin/pytest
-import sys
+from pydoc import cli
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import *
 
 g_send_times = 0
 g_recv_times = 0
 g_pub_times = 1
+g_connected = 0
+
+def on_log(client, userdata, level, buf):
+    print("log: ",buf)
 
 def on_message(self, obj, msg):
-    print("Receive:" + msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
     assert msg.topic == str(msg.payload, 'utf-8')
     global g_recv_times 
-    g_recv_times += 1
-
-def on_publish(self, obj, mid):
-    global g_send_times
     global g_pub_times
+    g_recv_times += 1
     if g_send_times == g_pub_times:
-        time.sleep(0.05)
         g_pub_times = 1
         self.disconnect()
     else:
         g_pub_times += 1
+
+
+# def on_connect(client_instance, userdata, flags, rc):
+#     global g_connected
+#     g_connected = 1
+
+def on_publish(self, obj, mid):
+    global g_send_times
+    global g_pub_times
+    # if g_send_times == g_pub_times:
+    #     time.sleep(5)
+    #     g_pub_times = 1
+    #     self.disconnect()
+    # else:
+    #     g_pub_times += 1
 
 class Test(object):
     def init(self, host="localhost", port=8083, tran='websockets', prot=MQTTv5):
@@ -29,27 +43,37 @@ class Test(object):
         self._port = port
         self._tran = tran
         self._prot = prot
-        self._mqttc = mqtt.Client(transport=self._tran, protocol=prot)   
-        self._mqttc.on_message = on_message
-        self._mqttc.on_publish = on_publish
     
-
     def assert_test(self, send_times, _qos, sub, pub):
         global g_send_times
         global g_recv_times
+        global g_connected
         g_recv_times = 0
         g_send_times = send_times
         qos = _qos
 
         # test sub
         while qos >= 0:
+            self._mqttc = mqtt.Client(transport=self._tran, protocol=self._prot)   
+            # self._mqttc.on_connect = on_connect
+            self._mqttc.on_message = on_message
+            self._mqttc.on_publish = on_publish
+            self._mqttc.on_log = on_log
+
             self._mqttc.connect(self._host, self._port, 60)
+            self._mqttc.loop_start()
+            # while g_connected == 0:
+            #     continue
             self._mqttc.subscribe(sub, qos)
             self._mqttc.publish(pub, pub, 0)
             self._mqttc.publish(pub, pub, 1)
             self._mqttc.publish(pub, pub, 2)
-            self._mqttc.loop_forever()
+            g_connected = 0
+            time.sleep(1)
+            self._mqttc.loop_stop()
+            self._mqttc.disconnect()
             qos -= 1
+
         assert g_send_times * (_qos + 1) == g_recv_times
 
         g_recv_times = 0
@@ -57,13 +81,25 @@ class Test(object):
 
         # test unsub
         while qos >= 0:
+            self._mqttc = mqtt.Client(transport=self._tran, protocol=self._prot)   
+            # self._mqttc.on_connect = on_connect
+            self._mqttc.on_message = on_message
+            self._mqttc.on_publish = on_publish
+            self._mqttc.on_log = on_log
+
             self._mqttc.connect(self._host, self._port, 60)
+            self._mqttc.loop_start()
+            # while g_connected == 0:
+            #     continue
             self._mqttc.subscribe(sub, qos)
             self._mqttc.unsubscribe(sub)
             self._mqttc.publish(pub, pub, 0)
             self._mqttc.publish(pub, pub, 1)
             self._mqttc.publish(pub, pub, 2)
-            self._mqttc.loop_forever()
+            g_connected = 0
+            time.sleep(1)
+            self._mqttc.loop_stop()
+            self._mqttc.disconnect()
             qos -= 1
         assert 0 == g_recv_times
 
