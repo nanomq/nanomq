760ef38f2948ee4911cee2ba7de3ac099232a85d
Jaylin <jaylin@emqx.io>
2022-09-29 15:06:29 +0800

    * NEW [bridge] add hybrid_bridge_client API

--- nanomq/include/bridge.h
+++ nanomq/include/bridge.h
@@ -2,9 +2,9 @@
 #define NANOMQ_BRIDGE_H
 
 #include "nng/mqtt/mqtt_client.h"
+#include "nng/nng.h"
 #include "nng/supplemental/nanolib/conf.h"
 #include "nng/supplemental/util/platform.h"
-#include "nng/nng.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,16 +12,18 @@ typedef struct {
 	nng_socket       *sock;
 	conf_bridge_node *config;
 	nng_mqtt_client  *client;
-	conf *            conf;
-	nng_mtx *         switch_mtx;
-	nng_cv *          switch_cv;
-	nng_mtx *         exec_mtx;
-	nng_cv *          exec_cv;
+	conf	     *conf;
+	nng_mtx          *switch_mtx;
+	nng_cv           *switch_cv;
+	nng_mtx          *exec_mtx;
+	nng_cv           *exec_cv;
 } bridge_param;
 
 extern bool topic_filter(const char *origin, const char *input);
 extern int  bridge_client(
      nng_socket *sock, conf *config, conf_bridge_node *bridge_conf);
+extern int hybrid_bridge_client(
+    nng_socket *sock, conf *config, conf_bridge_node *node);
 extern nng_msg *bridge_publish_msg(const char *topic, uint8_t *payload,
     uint32_t len, bool dup, uint8_t qos, bool retain, property *props);
 
