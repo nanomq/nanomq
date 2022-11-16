6367705c3822410957d061e60e04899e8bb85b29
alvin1221 <alvin@emqx.io>
2022-11-16 17:01:41 +0800

    * NEW [acl] add acl_handler.

--- nanomq/include/acl_handler.h
+++ nanomq/include/acl_handler.h
@@ -5,5 +5,7 @@
 #include "nng/supplemental/nanolib/conf.h"
 #include "nng/supplemental/nanolib/acl_conf.h"
 
+extern bool auth_acl(
+    conf *config, acl_action_type type, conn_param *param, const char *topic);
 
 #endif
