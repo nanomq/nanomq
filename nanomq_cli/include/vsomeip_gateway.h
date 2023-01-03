7aadbfd556e37b018dc46b129a5e2a99b9e1d9f3
lee-emqx <lihj@emqx.io>
2023-01-03 11:27:48 +0800

    * NEW [vsomeip_gateway] support vsomeip cmake option.

---
+++ nanomq_cli/include/vsomeip_gateway.h
@@ -0,0 +1,11 @@
+#ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
+#include <csignal>
+#endif
+#include <chrono>
+#include <condition_variable>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+#include <vsomeip/vsomeip.hpp>
