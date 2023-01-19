0bf6dbc7633f6eba8b242a1ee13829b4eb1e5128
alvin1221 <alvin@emqx.io>
2023-01-19 12:27:00 +0800

    * FIX [nanomq_cli] fix redirect stdout to file not work.

--- nanomq_cli/include/client.h
+++ nanomq_cli/include/client.h
@@ -6,5 +6,6 @@ extern int            subscribe_start(int argc, char **argv);
 extern int            connect_start(int argc, char **argv);
 extern struct topic **addtopic(struct topic **endp, const char *s);
 extern void           freetopic(struct topic *endp);
+extern void           console(const char *fmt, ...);
 
 #endif // NANOMQ_CLIENT_H
