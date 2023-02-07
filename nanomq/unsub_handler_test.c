fa87c358df196d208b4026208eda389b6a0664ce
Hermann0222 <yukang.wei@emqx.io>
2023-02-07 13:30:08 +0800

    * NEW [unsub_handler] add test for unsub_handler.

---
+++ nanomq/unsub_handler_test.c
@@ -0,0 +1,81 @@
+
+
+#include <assert.h>
+#include <stdio.h>
+
+#include "include/nanomq.h"
+#include "include/unsub_handler.h"
+
+int
+main()
+{
+	int rv = 0;
+
+	nano_work *work;
+	nng_socket sock;
+	conf      *nanomq_conf;
+
+	/* init work */
+	// sock.id = 0;
+	work = nng_alloc(sizeof(*work));
+	// work->pub_packet = NULL;
+	work->state     = INIT;
+	work->db        = NULL;
+	work->db_ret    = NULL;
+	work->proto     = PROTO_MQTT_BROKER;
+	work->proto_ver = MQTT_PROTOCOL_VERSION_v311;
+	work->config    = nanomq_conf;
+	// work->code       = SUCCESS;
+
+	// init msg.
+	nng_msg *msg;
+	nng_msg_alloc(&msg, 0);
+	uint8_t *payload_ptr, *variable_ptr, *fix_ptr;
+	size_t   remaining_len;
+	payload_ptr  = NULL;
+	variable_ptr = NULL;
+	// set topic for test:$MQTT.
+	uint8_t topic[] = { 0x00, 0x05, 0x24, 0x4D, 0x51, 0x54, 0x54, '\0' };
+	payload_ptr     = topic;
+	nng_msg_set_payload_ptr(msg, payload_ptr);
+	// set remaining_len.
+	remaining_len = nng_msg_len(msg);
+	nng_msg_set_remaining_len(msg, remaining_len);
+	// set work->msg.
+	work->msg = msg;
+	// alloc unsub_pkt.
+	work->unsub_pkt = nng_alloc(sizeof(packet_unsubscribe));
+
+	/* test for function decode_unsub_msg() */
+	uint8_t packet_id[2] = { 0x00, 0x05 };
+	nng_msg_append(work->msg, packet_id, 2);
+	remaining_len = 6;
+	nng_msg_set_remaining_len(work->msg, remaining_len);
+
+	rv = decode_unsub_msg(work);
+	assert(rv == 0);
+
+	topic_node *node = work->unsub_pkt->node;
+	rv = strcmp(node->topic.body, "$MQTT");
+
+	assert(rv == 0);
+
+	/* test for encode_unsuback_msg() */
+	uint8_t pkt_id;
+	uint8_t reason_code;
+	rv = encode_unsuback_msg(msg, work);
+	assert(rv == 0);
+
+	variable_ptr = nng_msg_body(msg);
+	NNI_GET16(variable_ptr, pkt_id);
+	assert(pkt_id == 5);
+
+	fix_ptr = nng_msg_header(msg);
+	assert(*(uint8_t *) fix_ptr == CMD_UNSUBACK);
+
+	// free unsub_pkt.
+	unsub_pkt_free(work->unsub_pkt);
+
+	nng_msg_free(msg);
+	nng_free(work, sizeof(struct work));
+}
