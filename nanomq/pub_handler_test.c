9b398daea28666f2d172035e3aff4ce32929ff31
Hermann0222 <yukang.wei@emqx.io>
2023-02-09 18:20:05 +0800

    * MDF [pub_handler test] format codes and update notes.

--- nanomq/pub_handler_test.c
+++ nanomq/pub_handler_test.c
@@ -14,35 +14,41 @@ main()
 	nano_work *work;
 	work            = nng_alloc(sizeof(*work));
 	work->proto_ver = MQTT_PROTOCOL_VERSION_v311;
-	nng_msg                  *msg;
-	struct pub_packet_struct *pub_packet;
-	nng_msg_alloc(&msg, 0);
-	pub_packet                         = nng_zalloc(sizeof(*pub_packet));
-	work->msg                          = msg;
-	work->pub_packet                   = pub_packet;
-	int                  remaining_len = 18;
-	struct fixed_header *fix_hd        = nng_alloc(sizeof(*fix_hd));
-	fix_hd->qos                        = 0;
-	fix_hd->packet_type                = PUBLISH;
-	int topic_len                      = 7;
-	int data_len                       = 4;
 
-	// topic:"$MQTT"
+	nng_msg *msg;
+	nng_msg_alloc(&msg, 0);
+
+	struct pub_packet_struct *pub_packet;
+	pub_packet                  = nng_zalloc(sizeof(*pub_packet));
+
+	work->msg                   = msg;
+	work->pub_packet            = pub_packet;
+
+	struct fixed_header *fix_hd = nng_alloc(sizeof(*fix_hd));
+	fix_hd->qos                 = 0;
+	fix_hd->packet_type         = PUBLISH;
+
+	// test data
+	uint32_t remaining_len      = 18;
+	uint32_t topic_len          = 7;
+	uint32_t data_len           = 4;
+	// topic: $MQTT
 	uint8_t topic[] = {
 		0x00, 0x05 /* topic length */, 0x24, 0x4D, 0x51, 0x54,
-		0x54 /* topic body*/,
-		// 0x00 /* topic option*/,
+		0x54 /* topic body*/
 	};
-	// data:"data"
+	// data: data
 	uint8_t data[] = { 0x64, 0x61, 0x74, 0x61 };
 
-	/* test for decode_pub_message */
-	// TODO test for MQTTv5
+	// init msg->body and msg->header
 	nng_msg_append(work->msg, topic, topic_len);
 	nng_msg_append(work->msg, data, data_len);
 	nng_msg_set_remaining_len(work->msg, remaining_len);
 	nng_msg_header_append(work->msg, fix_hd, sizeof(*fix_hd));
-	// test for remaining_len > msg_len.
+
+	/* test for decode_pub_message */
+	// TODO test for MQTTv5
+	// test for remaining_len > msg_len
 	rv_rc = decode_pub_message(work, MQTT_PROTOCOL_VERSION_v311);
 	assert(rv_rc == PROTOCOL_ERROR);
 	// test for commom case.
@@ -50,6 +56,7 @@ main()
 	nng_msg_set_remaining_len(work->msg, remaining_len);
 	rv_rc = decode_pub_message(work, MQTT_PROTOCOL_VERSION_v311);
 	assert(rv_rc == SUCCESS);
+	// check work->pub_packet
 	assert(work->pub_packet->var_header.publish.topic_name.len == 5);
 	assert(strcmp(work->pub_packet->var_header.publish.topic_name.body,
 	           "$MQTT") == 0);
@@ -57,28 +64,34 @@ main()
 	assert(strcmp(pub_packet->payload.data, "data") == 0);
 
 	/* test for encode_pub_message() */
+	// alloc dest_msg
 	nng_msg *dest_msg;
 	nng_msg_alloc(&dest_msg, 0);
 	nng_msg_set_cmd_type(dest_msg, CMD_PUBLISH);
 	rv_bool = encode_pub_message(dest_msg, work, PUBLISH);
 	assert(rv_bool == true);
 
-	// check dest_msg topic_len,topic_name,data.
+	// check dest_msg
+	// args to get the content of msg
 	uint32_t pos            = 0;
 	uint32_t dest_topic_len = 0;
 	uint8_t *dest_body      = nng_msg_body(msg);
+	// check topic len
 	NNI_GET16(dest_body + pos, dest_topic_len);
+	assert(dest_topic_len == 5);
 	pos += 2;
+	// check topic name
 	uint8_t *dest_topic = nng_zalloc(dest_topic_len + 1);
 	memcpy(dest_topic, (uint8_t *) (dest_body + pos), dest_topic_len);
 	assert(strcmp(dest_topic, "$MQTT") == 0);
 	pos += dest_topic_len;
+	// check data
 	uint32_t left_len  = nng_msg_len(dest_msg) - pos;
 	uint8_t *dest_data = nng_zalloc(left_len + 1);
 	memcpy(dest_data, (uint8_t *) (dest_body + pos), left_len);
 	assert(strcmp(dest_data, "data") == 0);
 
-	/* test for free_pub_packet(). */
+	/* test for free_pub_packet() */
 	free_pub_packet(pub_packet);
 
 	nng_free(dest_data, left_len);
