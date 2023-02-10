cc4b1dd26ff095cfe80696cbef9081cedb5fbdbd
Hermann0222 <yukang.wei@emqx.io>
2023-02-10 13:10:37 +0800

    * MDF [pub_handler] format and optimize test.

--- nanomq/pub_handler_test.c
+++ nanomq/pub_handler_test.c
@@ -15,18 +15,20 @@ main()
 	work            = nng_alloc(sizeof(*work));
 	work->proto_ver = MQTT_PROTOCOL_VERSION_v311;
 
-	nng_msg *msg;
+	nng_msg *msg, *tpcError_msg;
 	nng_msg_alloc(&msg, 0);
+	nng_msg_alloc(&tpcError_msg, 0);
 
-	struct pub_packet_struct *pub_packet;
-	pub_packet                  = nng_zalloc(sizeof(*pub_packet));
+	struct pub_packet_struct *pub_packet, *tpcError_pub_packet;
+	pub_packet          = nng_zalloc(sizeof(*pub_packet));
+	tpcError_pub_packet = nng_zalloc(sizeof(*tpcError_pub_packet));
 
-	work->msg                   = msg;
-	work->pub_packet            = pub_packet;
+	work->msg        = msg;
+	work->pub_packet = pub_packet;
 
 	struct fixed_header *fix_hd = nng_alloc(sizeof(*fix_hd));
-	fix_hd->qos                 = 0;
-	fix_hd->packet_type         = PUBLISH;
+	fix_hd->qos                  = 1;
+	fix_hd->packet_type          = PUBLISH;
 
 	// test data
 	uint32_t remaining_len      = 18;
@@ -34,40 +36,67 @@ main()
 	uint32_t data_len           = 4;
 	// topic: $MQTT
 	uint8_t topic[] = {
-		0x00, 0x05 /* topic length */, 0x24, 0x4D, 0x51, 0x54,
-		0x54 /* topic body*/
+		0x00, 0x05 /* topic length */,
+		0x24, 0x4D, 0x51, 0x54, 0x54 /* topic body*/
+	};
+	// topic: $MQT+
+	uint8_t topic_false[] = {
+		0x00, 0x05 /* topic length */,
+		0x24, 0x4D, 0x51, 0x54, 0x2B /* topic body*/
 	};
 	// data: data
 	uint8_t data[] = { 0x64, 0x61, 0x74, 0x61 };
+	// packetid: 5
+	uint8_t pkt_id[] = { 0x00, 0x05 };
 
 	// init msg->body and msg->header
-	nng_msg_append(work->msg, topic, topic_len);
-	nng_msg_append(work->msg, data, data_len);
-	nng_msg_set_remaining_len(work->msg, remaining_len);
-	nng_msg_header_append(work->msg, fix_hd, sizeof(*fix_hd));
+	nng_msg_append(msg, topic, topic_len);
+	nng_msg_append(msg, pkt_id, 2);
+	nng_msg_append(msg, data, data_len);
+	nng_msg_set_remaining_len(msg, remaining_len);
+	nng_msg_header_append(msg, fix_hd, sizeof(*fix_hd));
+
+	// init tpcError_msg->body
+	nng_msg_append(tpcError_msg, topic_false, topic_len);
+	// nng_msg_append(tpcError_msg, pkt_id, 2);
+	nng_msg_append(tpcError_msg, data, data_len);
+	nng_msg_set_remaining_len(tpcError_msg, 11);
+	nng_msg_header_append(tpcError_msg, fix_hd, sizeof(*fix_hd));
+
 
 	/* test for decode_pub_message */
 	// TODO test for MQTTv5
 	// test for remaining_len > msg_len
 	rv_rc = decode_pub_message(work, MQTT_PROTOCOL_VERSION_v311);
 	assert(rv_rc == PROTOCOL_ERROR);
+
 	// test for commom case.
-	remaining_len = 11;
-	nng_msg_set_remaining_len(work->msg, remaining_len);
+	remaining_len = 13;
+	nng_msg_set_remaining_len(msg, remaining_len);
 	rv_rc = decode_pub_message(work, MQTT_PROTOCOL_VERSION_v311);
 	assert(rv_rc == SUCCESS);
 	// check work->pub_packet
 	assert(work->pub_packet->var_header.publish.topic_name.len == 5);
-	assert(strcmp(work->pub_packet->var_header.publish.topic_name.body,
-	           "$MQTT") == 0);
+	assert(strcmp(work->pub_packet->var_header.publish.topic_name.body, "$MQTT") == 0);
 	assert(work->pub_packet->payload.len == 4);
 	assert(strcmp(pub_packet->payload.data, "data") == 0);
+	assert(work->pub_packet->var_header.publish.packet_id == 5);
+
+	// test for wrong topic body
+	work->msg = tpcError_msg;
+	work->pub_packet = tpcError_pub_packet;
+	rv_rc = decode_pub_message(work, MQTT_PROTOCOL_VERSION_v311);
+	assert(rv_rc == PROTOCOL_ERROR);
+
 
 	/* test for encode_pub_message() */
-	// alloc dest_msg
+	// alloc dest_msg and init work
 	nng_msg *dest_msg;
 	nng_msg_alloc(&dest_msg, 0);
 	nng_msg_set_cmd_type(dest_msg, CMD_PUBLISH);
+	work->msg = msg;
+	work->pub_packet = pub_packet;
+
 	rv_bool = encode_pub_message(dest_msg, work, PUBLISH);
 	assert(rv_bool == true);
 
@@ -85,20 +114,36 @@ main()
 	memcpy(dest_topic, (uint8_t *) (dest_body + pos), dest_topic_len);
 	assert(strcmp(dest_topic, "$MQTT") == 0);
 	pos += dest_topic_len;
+	// check pkt_id
+	uint32_t dest_pkt_id = 0;
+	NNI_GET16(dest_body + pos, dest_pkt_id);
+	assert(dest_pkt_id == 5);
+	pos += 2;
 	// check data
 	uint32_t left_len  = nng_msg_len(dest_msg) - pos;
 	uint8_t *dest_data = nng_zalloc(left_len + 1);
 	memcpy(dest_data, (uint8_t *) (dest_body + pos), left_len);
 	assert(strcmp(dest_data, "data") == 0);
 
+
 	/* test for free_pub_packet() */
 	free_pub_packet(pub_packet);
+	free_pub_packet(tpcError_pub_packet);
 
+
+	/* test for init_pipe_content() */
+	struct pipe_content *pipe_ct = nng_zalloc(sizeof(*pipe_ct));
+	init_pipe_content(pipe_ct);
+	assert(pipe_ct->msg_infos == NULL);
+
+
+	nng_free(pipe_ct,sizeof(*pipe_ct));
 	nng_free(dest_data, left_len);
 	nng_free(dest_topic, dest_topic_len);
 	nng_free(fix_hd, sizeof(*fix_hd));
 	nng_msg_free(dest_msg);
 	nng_msg_free(msg);
+	nng_msg_free(tpcError_msg);
 	nng_free(work, sizeof(*work));
 
 	return SUCCESS;
