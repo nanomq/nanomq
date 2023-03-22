1d1998d9cab88f9d0f17ea0ca1d72245c3a54bbb
alvin1221 <alvin@emqx.io>
2023-03-22 18:14:21 +0800

    * DEL [dds] remove unused macros

--- nanomq_cli/include/dds_client.h
+++ nanomq_cli/include/dds_client.h
@@ -9,15 +9,19 @@
 #include "vector.h"
 #include "mqtt_client.h"
 
-#define DDS_TYPE_NAME_CAT1(A, B) A ## B
-#define DDS_TYPE_NAME_CAT(A, B) DDS_TYPE_NAME_CAT1(A, B)
+// #define DDS_TYPE_NAME_CAT1(A, B) A ## B
+// #define DDS_TYPE_NAME_CAT(A, B) DDS_TYPE_NAME_CAT1(A, B)
 
-#define DDS_TYPE_NAME_FREE1(x, y) do {\
-	DDS_TYPE_NAME_CAT(DDS_TYPE_NAME, _free)(x, y); \
-} while (0)
-#define DDS_TYPE_NAME_FREE(x, y)  DDS_TYPE_NAME_FREE1(x, y)
-#define DDS_TYPE_NAME_ALLOC() DDS_TYPE_NAME_CAT(DDS_TYPE_NAME, __alloc())
-#define DDS_TYPE_NAME_DESC()  DDS_TYPE_NAME_CAT(DDS_TYPE_NAME, _desc)
+// #define DDS_TYPE_NAME_FREE1(x, y) do {\
+// 	DDS_TYPE_NAME_CAT(DDS_TYPE_NAME, _free)(x, y); \
+// } while (0)
+// #define DDS_TYPE_NAME_FREE(x, y) DDS_TYPE_NAME_FREE1(x, y)
+// #define DDS_TYPE_NAME_ALLOC() DDS_TYPE_NAME_CAT(DDS_TYPE_NAME, __alloc())
+// #define DDS_TYPE_NAME_DESC() DDS_TYPE_NAME_CAT(DDS_TYPE_NAME, _desc)
+
+// #define DDS_DATA_FREE(name, func) DDS_TYPE_NAME_FREE(name, func)
+// #define DDS_DATA_ALLOC(name) DDS_TYPE_NAME_CAT(name, __alloc())
+// #define DDS_DATA_DESC(name) DDS_TYPE_NAME_CAT(name, _desc())
 
 typedef struct dds_cli dds_cli;
 
