091189f626ebcd3eca8acefa483f8d390fe07bab
Hermann0222 <yukang.wei@emqx.io>
2023-04-24 18:42:14 +0800

    * ADD [broker_test] more notes for test of broker_reload.

--- nanomq/apps/broker_test.c
+++ nanomq/apps/broker_test.c
@@ -10,8 +10,11 @@ int main()
 	rv = broker_start(0, NULL);
 	assert(rv == 0);
 
-	// rv = broker_reload(0, NULL);
-	// assert(rv == 0);
+	// broker_reload() is not supported since internal ipc is disabled
+	// default.
+
+	//  rv = broker_reload(0, NULL);
+	//  assert(rv == 0);
 
 	rv = broker_dflt(0, NULL);
 	assert(rv == 0);
