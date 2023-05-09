f4c8d77bb7abb10d76cceafc4c51181984f05ed4
Hermann0222 <yukang.wei@emqx.io>
2023-05-09 11:34:49 +0800

    * FIX [test] simplify nmq broker test.

--- nanomq/apps/broker_test.c
+++ nanomq/apps/broker_test.c
@@ -46,98 +46,41 @@ popen2(const char *command, int *infp, int *outfp)
     return pid;
 }
 
-int main()
+uint32_t
+main()
 {
-	int rv = 0;
+	uint32_t rv = 0;
 
-	// char *nmq_start =
-	//     "nanomq start --url tls+nmq-tcp://0.0.0.0:8883 --http --cacert "
-	//     "etc/certs/cacert.pem --cert etc/certs/cert.pem --key "
-	//     "etc/certs/key.pem --qos_duration 1 --log_level debug  "
-	//     "--log_stdout false";
-	// int    argc      = 4;
-	// char **nmq_start = { "nanomq", "start", "--url",
-	// 	"tls+nmq-tcp://0.0.0.0:8883" };
+	char *cmd_sub = "mosquitto_sub -h 127.0.0.1 -p 1883 -t topic -q 1";
+	char *cmd_pub = "mosquitto_pub -h 127.0.0.1 -p 1883 -t topic -m message -q 1";
 
-	char *cli_sub = "mosquitto_sub -h 127.0.0.1 -p 1883 -t topic -q 1";
-	char *cli_pub = "mosquitto_pub -h 127.0.0.1 -p 1883 -t topic -m message -q 1";
+	// char *cmd_sub = "mosquitto_sub -h 116.205.239.134 -p 1883 -t topic -q 1";
+	// char *cmd_pub = "mosquitto_pub -h 116.205.239.134 -p 1883 -t topic -m massage -q 1";
 
+	pthread_t nmq;
+	pid_t pid_sub;
+	FILE *p_pub = NULL;
 
-	// char *cli_sub = "mosquitto_sub -h 116.205.239.134 -p 1883 -t topic -q 1";
-	// char *cli_pub = "mosquitto_pub -h 116.205.239.134 -p 1883 -t topic -m massage -q 1";
-
-	int buf_size = 32;
+	uint32_t buf_size = 128;
 	char buf[buf_size];
+	uint32_t infp, outfp;
 
-	int infp, outfp;
+	// create nmq thread
+	pthread_create(&nmq, NULL, broker_start, NULL);
+	nng_msleep(50); // wait a while for broker to fully start
 
-	pthread_t tidp;
-	pid_t subpid, pubpid;
-	pthread_create(&tidp, NULL, broker_start, NULL);
-	nng_msleep(20);
+	// pipe to sub
+	pid_sub = popen2(cmd_sub, &infp, &outfp);
+	nng_msleep(50); // pub should be slightly behind sub
+	// pipe to pub
+	p_pub   = popen(cmd_pub, "r");
 
-	FILE *p_file, *p_subfile, *p_pubfile, *p_unsubfile = NULL;
-	printf("aaaaaaaa\n");
-	// p_file      = popen(nmq_start, "r");
-	subpid = popen2(cli_sub, &infp, &outfp);
-	// p_subfile   = popen(cli_sub, "r");
-	printf("aaaaaaaa\n");
-	p_pubfile   = popen(cli_pub, "r");
-	// system(cli_sub);
-	// printf("aaaaaaaa\n");
-	// system(cli_pub);
-
-	printf("aaaaaaaa\n");
-	nng_msleep(1000);
-	// pthread_kill(tidp, 0);
-	// pthread_cancel(tidp);
-
-	// if(fgets(buf, buf_size, p_subfile) != NULL) {
-	// 	assert(strncmp(buf, "message", 7) == 0);
-	// 	fprintf(stdout, "----------------------------%s", buf);
-	// }
-
-	// write(infp, "hello\n", 4);
-	// close(infp);
-	// *buf = '\0';
+	// check recv msg
 	read(outfp, buf, buf_size);
+	assert(strncmp(buf, "message", 7) == 0);
 
-	printf("\t%d\n", subpid);
-
-	// assert(strncmp(buf, "message", 7) == 0);
-	fprintf(stdout, "----------------------------%s\n", buf);
-
-	// pthread_join(tidp, NULL);
-	printf("aaaaaaaa\n");
-	nng_msleep(200);
-
-	kill(subpid, SIGKILL);
-	// kill(subpid, SIGKILL);
-
-	pclose(p_pubfile);
-	printf("aaaaaaaa\n");
-	// pclose(p_subfile);
-	// printf("aaaaaaaa\n");
-	// pthread_exit(&rv);
-
-	// pthreaddes
-	
-	// pclose(p_file);
-
-	// rv = broker_start(0, NULL);
-	// broker_stop(0, NULL);
-	// assert(rv == 0);
-
-	// broker_stop(0, NULL);
-
-	// // broker_reload() is not supported since internal ipc is disabled
-	// // default.
-
-	// //  rv = broker_reload(0, NULL);
-	// //  assert(rv == 0);
-
-	// rv = broker_dflt(0, NULL);
-	// assert(rv == 0);
+	kill(pid_sub, SIGKILL);
+	pclose(p_pub);
 
 	return 0;
 }
