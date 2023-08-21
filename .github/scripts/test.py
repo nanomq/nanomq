e0b75a7e746006747e993e3e86ae20932a3e3d05
Moi Ran <maoyi.ran@emqx.io>
2023-08-21 16:16:08 +0800

    * MDF [CI] chang log level fot test
    Signed-off-by: Moi Ran <maoyi.ran@emqx.io>

--- .github/scripts/test.py
+++ .github/scripts/test.py
@@ -17,7 +17,7 @@ from rest_api_test import rest_api_test
 from vulnerability_test import vul_test
 
 nanomq_log_path = "/tmp/nanomq_test.log" 
-nanomq_cmd = "nanomq start --url tls+nmq-tcp://0.0.0.0:8883 --http --cacert etc/certs/cacert.pem --cert etc/certs/cert.pem --key etc/certs/key.pem --qos_duration 1 --log_level debug  --log_stdout false --log_file /tmp/nanomq_test.log"
+nanomq_cmd = "nanomq start --url tls+nmq-tcp://0.0.0.0:8883 --http --cacert etc/certs/cacert.pem --cert etc/certs/cert.pem --key etc/certs/key.pem --qos_duration 1 --log_level trace  --log_stdout false --log_file /tmp/nanomq_test.log"
 
 def print_nanomq_log():
     log_lines = open(nanomq_log_path, 'r')
