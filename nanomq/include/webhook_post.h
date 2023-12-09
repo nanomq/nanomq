91ab2909b8743229cb84607d3caf5f187edbe701
jaylin <jaylin@emqx.io>
2023-12-09 14:43:51 +0800

    * MDF [exchange] rename hook_entry func & rm exchange.c
    Signed-off-by: jaylin <jaylin@emqx.io>

--- nanomq/include/webhook_post.h
+++ nanomq/include/webhook_post.h
@@ -13,5 +13,5 @@ extern int webhook_client_connack(nng_socket *sock, conf_web_hook *hook_conf,
 extern int webhook_client_disconnect(nng_socket *sock,
     conf_web_hook *hook_conf, uint8_t proto_ver, uint16_t keepalive,
     uint8_t reason, const char *username, const char *client_id);
-extern int webhook_entry(nano_work *work, uint8_t reason);
+extern int hook_entry(nano_work *work, uint8_t reason);
 #endif
