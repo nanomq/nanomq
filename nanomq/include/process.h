2ca1eec67b986f85999bdcd5aa8988acdefc2273
jaylin <jaylin@emqx.io>
2023-12-19 17:58:32 +0800

    * MDF [nanomq/lib] add missing header include files
    Signed-off-by: jaylin <jaylin@emqx.io>

--- nanomq/include/process.h
+++ nanomq/include/process.h
@@ -1,6 +1,14 @@
+#ifndef PROCESS_H
+#define PROCESS_H
 
-int process_is_alive(int pid);
-int process_send_signal(int pid, int signal);
-int pidgrp_send_signal(int pid, int signal);
-int process_daemonize(void);
-int process_create_child(int (*child_run)(void *), void *data);
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+extern int process_is_alive(int pid);
+extern int process_send_signal(int pid, int signal);
+extern int pidgrp_send_signal(int pid, int signal);
+extern int process_daemonize(void);
+extern int process_create_child(int (*child_run)(void *), void *data);
+
+#endif
