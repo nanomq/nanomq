bbe17d6bb1e3cf965beadf670f775a7e027f631a
lee-emqx <lihj@emqx.io>
2024-01-29 15:45:46 +0800

    * NEW [nanomq_test] add nanomq_test.h
    Signed-off-by: lee-emqx <lihj@emqx.io>

---
+++ nanomq/tests/nanomq_test.h
@@ -0,0 +1,36 @@
+#ifndef __NANOMQ_TEST_H__
+#define __NANOMQ_TEST_H__
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+// Structure allocation conveniences.
+#define ALLOC_STRUCT(s) nng_zalloc(sizeof(*s))
+#define FREE_STRUCT(s) nng_free((s), sizeof(*s))
+#define ALLOC_STRUCTS(s, n) nng_zalloc(sizeof(*s) * n)
+#define FREE_STRUCTS(s, n) nng_free(s, sizeof(*s) * n)
+
+#define clean_errno() (errno == 0 ? "None" : strerror(errno))
+
+#define log_err(M, ...) fprintf(stderr,\
+        "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
+        clean_errno(), ##__VA_ARGS__)
+
+#define log_test(M, ...) fprintf(stderr,\
+        "[INFO] (%s:%d: errno: %s) " M "\n",\
+        __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+
+#define check(A, M, ...) if(!(A)) {\
+    log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+
+#define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__);\
+    errno=0; goto error; }
+
+#define check_mem(A) check((A), "Out of memory.")
+
+#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__);\
+    errno=0; goto error; }
+
+
+#endif // __NANOMQ_TEST_H__
