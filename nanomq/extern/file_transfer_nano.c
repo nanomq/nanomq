7a07916a591082002e894859cc6aa1ad39de1d68
wanghaemq <wangwei@emqx.io>
2024-01-29 06:01:42 -0500

    * NEW [extern] Add func to get md5 result of a binary.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

--- nanomq/extern/file_transfer_nano.c
+++ nanomq/extern/file_transfer_nano.c
@@ -175,6 +175,26 @@ static int publish_file(nng_socket *sock, FILE *fp, char *file_name, char *topic
 #define PATH_LEN 256
 #define MD5_LEN 32
 
+int CalcMD5(char *binary, char *tmp_fname, char *md5_sum)
+{
+	#define MD5SUM_CMD_FMT "md5sum %." STR(PATH_LEN) "s 2>/dev/null"
+	char cmd[PATH_LEN + sizeof (MD5SUM_CMD_FMT)];
+	sprintf(cmd, MD5SUM_CMD_FMT, tmp_fname);
+	#undef MD5SUM_CMD_FMT
+
+	FILE *p = popen(cmd, "r");
+	if (p == NULL) return 0;
+
+	int i, ch;
+	for (i = 0; i < MD5_LEN && isxdigit(ch = fgetc(p)); i++) {
+		*md5_sum++ = ch;
+	}
+
+	*md5_sum = '\0';
+	pclose(p);
+	return i == MD5_LEN;
+}
+
 int CalcFileMD5(char *file_name, char *md5_sum)
 {
 	#define MD5SUM_CMD_FMT "md5sum %." STR(PATH_LEN) "s 2>/dev/null"
