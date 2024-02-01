609c8f1397e6310013225c3dcc7588bae8b90acb
wanghaemq <wangwei@emqx.io>
2024-01-31 22:42:05 -0500

    * NEW [hook] Add process of decrpytion.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

--- nanomq/webhook_inproc.c
+++ nanomq/webhook_inproc.c
@@ -74,6 +74,103 @@ static const int  aes_gcm_aad_sz = 16;
 static const char aes_gcm_iv[] =
 {0x99, 0xaa, 0x3e, 0x68, 0xed, 0x81, 0x73, 0xa0, 0xee, 0xd0, 0x66, 0x84};
 
+char* aes_gcm_decrypt(char *ciphertext, int ciphertext_len,
+		char *key, char **tagp, int *plaintext_lenp)
+{
+	const EVP_CIPHER *cipher_handle;
+	switch (strlen(key) * 8) {
+	case 128:
+		cipher_handle = EVP_aes_128_gcm();
+		break;
+	case 192:
+		cipher_handle = EVP_aes_192_gcm();
+		break;
+	case 256:
+		cipher_handle = EVP_aes_256_gcm();
+		break;
+	default:
+		log_error("Unsupported aes key length");
+		return NULL;
+	}
+
+	char *plaintext = malloc(sizeof(char) * (ciphertext_len+32));
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int plaintext_len;
+    int ret;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())) {
+		log_error("error in new ctx");
+		return NULL;
+	}
+
+    /* Initialise the decryption operation. */
+    if(!EVP_DecryptInit_ex(ctx, cipher_handle, NULL, NULL, NULL)) {
+		log_error("error in init ctx");
+		return NULL;
+	}
+
+    /* Set IV length. Not necessary if this is 12 bytes (96 bits) */
+    if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, sizeof(aes_gcm_iv), NULL)) {
+		log_error("error in ctx ctrl");
+		return NULL;
+	}
+
+    /* Initialise key and IV */
+    if(!EVP_DecryptInit_ex(ctx, NULL, NULL, key, aes_gcm_iv)) {
+		log_error("error in decrypted init");
+		return NULL;
+	}
+
+    /*
+     * Provide any AAD data. This can be called zero or more times as
+     * required
+     */
+    if(!EVP_DecryptUpdate(ctx, NULL, &len, aes_gcm_aad, aes_gcm_aad_sz)) {
+		log_error("error in decrypted update1");
+		return NULL;
+	}
+
+    /*
+     * Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary
+     */
+    if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) {
+		log_error("error in decrypted update1");
+		return NULL;
+	}
+    plaintext_len = len;
+
+	char *tag = malloc(sizeof(char) * 16);
+    /* Set expected tag value. Works in OpenSSL 1.0.1d and later */
+    if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag)) {
+		log_error("error in ctx ctrl2");
+		return NULL;
+	}
+	*tagp = tag;
+
+    /*
+     * Finalise the decryption. A positive return value indicates success,
+     * anything else is a failure - the plaintext is not trustworthy.
+     */
+    ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    if(ret > 0) {
+        /* Success */
+        plaintext_len += len;
+		*plaintext_lenp = plaintext_len;
+        return plaintext;
+    } else {
+		log_error("error in decryption %d", ret);
+        /* Verify failed */
+        return NULL;
+    }
+}
+
 static char *
 aes_gcm_encrypt(char *plain, int plainsz, char *key, char **tagp, int *cipher_lenp)
 {
@@ -140,6 +237,7 @@ aes_gcm_encrypt(char *plain, int plainsz, char *key, char **tagp, int *cipher_le
 
 	EVP_CIPHER_CTX_free(ctx);
 	*cipher_lenp = cipher_len;
+
 	return buf;
 err:
 	log_error("AES GCM error code %d", res);
@@ -187,6 +285,17 @@ send_mqtt_msg_cat(nng_socket *sock, char *tmpfpath, nng_msg **msgs, uint32_t len
 		nng_free(buf, pos);
 		return -1;
 	}
+	log_info("encryption result: %s (%d)", cipher, cipher_len);
+
+	int   plain_len;
+	char *plain = aes_gcm_decrypt(cipher, cipher_len, key, &tag, &plain_len);
+	if (plain == NULL) {
+		log_error("error in aes gcm encryption");
+		nng_msg_free(pubmsg);
+		nng_free(buf, pos);
+		return -1;
+	}
+	log_info("decryption result: %s (%d)", plain, plain_len);
 
 	char *md5sum;
 	if (0 != CalcMD5n(cipher, cipher_len, tmpfpath, &md5sum)) {
