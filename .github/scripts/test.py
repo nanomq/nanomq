5e1910163d8dffdf85f47cd2334e33f03d717c1c
wayne <yukang.wei@emqx.io>
2024-04-27 17:09:25 +0800

    * FIX [workflow] fix GET too frequently makes the server to close connection without response
    Signed-off-by: wayne <yukang.wei@emqx.io>

--- .github/scripts/test.py
+++ .github/scripts/test.py
@@ -87,7 +87,6 @@ if __name__=='__main__':
         raise AssertionError
     print("fuzzy test end")
 
-    time.sleep(1)
     print("rest api test start")
     if False == rest_api_test():
         nanomq.terminate()
