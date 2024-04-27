99c710bdcc4894ee1f7ed57d227cf0be11ece741
wayne <yukang.wei@emqx.io>
2024-04-27 18:00:50 +0800

    * MDF [workflow] fix topic alias
    Signed-off-by: wayne <yukang.wei@emqx.io>

--- .github/scripts/mqtt_test_v5.py
+++ .github/scripts/mqtt_test_v5.py
@@ -204,6 +204,7 @@ def test_topic_alias():
         time.sleep(1)
         times += 1
     
+    time.sleep(2)
     process1.terminate()
     os.kill(pid.value, signal.SIGKILL)
     if cnt.value == 10:
