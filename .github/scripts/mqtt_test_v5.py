f62bdf39ceb4d82498dfeac000687d59f3110b12
wayne <yukang.wei@emqx.io>
2024-05-08 14:08:42 +0800

    * MDF [workflow] revert changes
    Signed-off-by: wayne <yukang.wei@emqx.io>

--- .github/scripts/mqtt_test_v5.py
+++ .github/scripts/mqtt_test_v5.py
@@ -192,7 +192,7 @@ def test_topic_alias():
     pid = Value('i', 0)
     process1 = Process(target=cnt_message, args=(sub_cmd, cnt, pid, "message"))
     process1.start()
-    time.sleep(2)
+    time.sleep(1)
     process2 = subprocess.Popen(pub_cmd,
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
@@ -204,7 +204,6 @@ def test_topic_alias():
         time.sleep(1)
         times += 1
     
-    time.sleep(2)
     process1.terminate()
     os.kill(pid.value, signal.SIGKILL)
     if cnt.value == 10:
