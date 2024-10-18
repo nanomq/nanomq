0f54c15cf7bb614df542e2594c07137e864012b5
xinyi-xs <lihj@emqx.io>
2024-10-18 16:16:13 +0800

    * NEW [deploy] add nanomq android build script.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

---
+++ deploy/lianyou-deploy/build_nanomq_ndk.sh
@@ -0,0 +1,41 @@
+
+#!/bin/bash
+
+INSTALL_DIR=${1:-"/tmp"}
+NDK_VERSION="android-ndk-r23c"
+NDK_DIR="${INSTALL_DIR}/${NDK_VERSION}"
+NDK_URL="https://assets.emqx.com/data/ndk/android-ndk-r23c.tar.gz"
+
+export ANDROID_NDK=$NDK_DIR
+export ANDROID_NDK_HOME=$NDK_DIR
+export PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+
+
+wget ${NDK_URL} -O ${NDK_DIR}.tar.gz
+
+
+echo "Start clean build_android"
+rm -rf build_android
+mkdir build_android
+cd build_android
+
+echo "Start build for Android"
+
+cmake -DANDROID_PLATFORM=android-30 \
+    -DANDROID_ABI=arm64-v8a \
+    -DCMAKE_TOOLCHAIN_FILE=$NDK_DIR/build/cmake/android.toolchain.cmake \
+    -DENABLE_PARQUET=ON \
+    -DANDROID_STL=c++_shared \
+    -DNNG_ENABLE_QUIC=ON \
+    -DENABLE_FILETRANSFER=ON \
+    ..
+
+echo "Start make"
+make -j1
+
+
+echo "Build done" 
+
+mkdir lib
+cp $NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/sysroot/lib/* lib
+cp ./src/supplemental/quic/msquic/msquic/bin/Release/libmsquic.so lib
