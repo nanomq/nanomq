f9baaec042874fadd2c64bc74015e7aa4db92c6c
xinyi-xs <lihj@emqx.io>
2025-03-17 17:38:37 +0800

    * NEW [deploy] add build_nanomq_ndk-e02.sh.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

---
+++ deploy/lianyou-deploy/build_nanomq_ndk-e02.sh
@@ -0,0 +1,47 @@
+
+#!/bin/bash
+
+INSTALL_DIR=${1:-"/tmp"}
+NDK_VERSION="android-ndk-r19c"
+NDK_DIR="${INSTALL_DIR}/${NDK_VERSION}"
+NDK_URL="https://assets.emqx.com/data/ndk/android-ndk-r19c.tar.gz"
+
+export ANDROID_NDK=$NDK_DIR
+export ANDROID_NDK_HOME=$NDK_DIR
+export PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+
+
+wget ${NDK_URL} -O ${NDK_DIR}.tar.gz
+tar -zxvf ${NDK_DIR}.tar.gz -C /tmp
+
+
+echo "Start clean build_android"
+rm -rf build_android
+mkdir build_android
+cd build_android
+
+echo "Start build for Android"
+
+cmake -DANDROID_PLATFORM=android-28 \
+    -DANDROID_ABI=arm64-v8a \
+    -DCMAKE_TOOLCHAIN_FILE=$NDK_DIR/build/cmake/android.toolchain.cmake \
+    -DENABLE_PARQUET=ON \
+    -DANDROID_STL=c++_shared \
+    -DNNG_ENABLE_QUIC=OFF \
+    -DENABLE_FILETRANSFER=ON \
+    -DENABLE_PARQUET_SHARED=ON \
+    -DNNG_ENABLE_TLS=ON \
+    ..
+
+echo "Start make"
+make -j1
+
+
+echo "Build done" 
+
+mkdir lib
+cp $NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/sysroot/lib/*.so lib
+# cp ./nng/src/supplemental/quic/msquic/msquic/bin/Release/libmsquic.so lib
+
+echo "Clear Ndk"
+rm -rf $NDK_DIR ${NDK_DIR}.tar.gz
