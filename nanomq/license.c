05e7905bbcafa6acdefabea10435021c400b7b1e
wanghaemq <wangwei@emqx.io>
2025-07-01 03:35:26 -0400

    * NEW [license] New license includes more fields.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

--- nanomq/license.c
+++ nanomq/license.c
@@ -10,9 +10,12 @@
 static const char *lic_path = NULL;
 static char *lic_key = "202505121520dcba";
 
+#define MAX_LIC_BUF_LEN 430
+
 static int
 lic_dec(char *data, size_t sz,
-		uint32_t *total, uint32_t *cur, uint32_t *nstart, uint32_t *nend)
+		uint32_t *vm, uint32_t *um, char *st, char *mode,
+		char *name, char *email, char *dc, uint32_t *lc)
 {
 	char *out = NULL;
 	int   out_sz;
@@ -21,9 +24,10 @@ lic_dec(char *data, size_t sz,
 		log_error("license dec failed");
 		return -1;
 	}
-	uint32_t d1, d2, d3, d4;
-	int num = sscanf(out, "%u,%u,%u,%u", total, cur, nstart, nend);
-	if (num != 4) {
+	log_info("(%d)%s", out_sz, out);
+	// valid minutes,used minutes,start time,trial/official,name,email,dc,limit connections
+	int num = sscanf(out, "%u,%u,%s,%s,%s,%s,%s,%u", vm, um, st, mode, name, email, dc, lc);
+	if (num != 8) {
 		nng_free(out, 0);
 		log_error("license content is malformed rv%d", num);
 		return -2;
@@ -33,14 +37,15 @@ lic_dec(char *data, size_t sz,
 }
 
 static int
-lic_enc(uint32_t total, uint32_t cur, uint32_t nstart, uint32_t nend,
+lic_enc(uint32_t vm, uint32_t um, char *st, char *mode,
+		char *name, char *email, char *dc, uint32_t lc,
 		char *cipher, size_t *sz)
 {
-	char  buf[64];
+	char  buf[MAX_LIC_BUF_LEN];
 	char *tag;
 	int   out_sz;
 	char *out = NULL;
-	sprintf(buf, "%u,%u,%u,%u\n", total, cur, nstart, nend);
+	sprintf(buf, "%u,%u,%s,%s,%s,%s,%s,%u\n", vm, um, st, mode, name, email, dc, lc);
 	if (NULL == (out = aes_gcm_encrypt(buf, strlen(buf), lic_key, &tag, &out_sz))) {
 		log_error("license enc failed");
 		return -1;
@@ -65,23 +70,29 @@ lic_init(const char *path)
 		log_error("license(%s) read failed %d(%s)", lic_path, rv, nng_strerror(rv));
 		return -1;
 	}
-	if (data == NULL || sz == 0 || sz > 128 || sz < 36) {
+	// max 12 + 12 + 8 + 8 + 128 + 128 + 128 + 6
+	if (data == NULL || sz == 0 || sz > MAX_LIC_BUF_LEN || sz < 20) {
 		log_error("license(%s) empty or file has a invalid sz%ld", lic_path, sz);
 		return -1;
 	}
-	uint32_t nstart = 0, nend = 0, cur = 0, total = 0;
-	if (0 != lic_dec(data, sz, &total, &cur, &nstart, &nend)) {
+	uint32_t vm = 0, um = 0, lc = 0;
+	char st[9] = {0};
+	char mode[9] = {0};
+	char name[129] = {0};
+	char email[129] = {0};
+	char dc[129] = {0};
+	if (0 != lic_dec(data, sz, &vm, &um, st, mode, name, email, dc, &lc)) {
 		nng_free(data, 0);
 		log_error("license(%s) is malformed", lic_path);
 		return -1;
 	}
 	nng_free(data, 0);
-	if (cur > total || nstart > nend) {
+	if (um > vm) {
 		log_error("license(%s) is expired", lic_path);
 		return -2;
 	} else {
 #if defined(DEBUG)
-		log_info("license is active %ld/%ld", cur, total);
+		log_info("license is active %ld/%ld", um, vm);
 #else
 		log_info("license is active");
 #endif
@@ -104,28 +115,31 @@ lic_update(uint32_t addon)
 		log_error("license(%s) read failed rv%d(%s)", lic_path, rv, nng_strerror(rv));
 		return -1;
 	}
-	if (data == NULL || sz == 0) {
-		log_error("license(%s) empty", lic_path);
+	// max 12 + 12 + 8 + 8 + 128 + 128 + 128 + 6
+	if (data == NULL || sz == 0 || sz > 430 || sz < 20) {
+		log_error("license(%s) empty or file has a invalid sz%ld", lic_path, sz);
 		return -1;
 	}
-	uint32_t nstart = 0, nend = 0, cur = 0, total = 0;
-	if (0 != (rv = lic_dec(data, sz, &total, &cur, &nstart, &nend))) {
-		nng_free(data, 0);
+	uint32_t vm = 0, um = 0, lc = 0;
+	char st[9], mode[9], name[129], email[129], dc[129];
+	if (0 != (rv = lic_dec(data, sz, &vm, &um, st, mode, name, email, dc, &lc))) {
 		log_error("license(%s) decode failed rv%d", lic_path, rv);
 		return -1;
 	}
 	nng_free(data, 0);
 
-	cur += addon;
+	um += addon;
 
 #if defined(DEBUG)
-	log_debug("license updated %ld/%ld %ld-%ld", cur, total, nstart, nend);
+	log_info("license updated %ld/%ld st:%s mode:%s %s-%s-%s lc:%d",
+			um, vm, st, mode, name, email, dc, lc);
 #endif
 
 	// WRITE license
 	char  *cipher = nng_alloc(sz + 2);
 	size_t cipher_sz;
-	if (0 != (rv = lic_enc(total, cur, nstart, nend, cipher, &cipher_sz))) {
+
+	if (0 != (rv = lic_enc(vm, um, st, mode, name, email, dc, lc, cipher, &cipher_sz))) {
 		nng_free(cipher, sz + 2);
 		log_error("license(%s) encode failed rv%d", lic_path, rv);
 		return -3;
@@ -137,7 +151,7 @@ lic_update(uint32_t addon)
 	}
 	nng_free(cipher, sz + 2);
 
-	if (cur > total || nstart > nend) {
+	if (um > vm) {
 		log_error("license(%s) is expired", lic_path);
 		return -2;
 	}
