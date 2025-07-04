860858cf83d17f7f8294bca7ded69664cd27af5d
wanghaemq <wangwei@emqx.io>
2025-07-04 05:16:56 -0400

    * NEW [license] Fix the key of license type and limit connections in cert ext.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

--- nanomq/include/license_std.h
+++ nanomq/include/license_std.h
@@ -3,8 +3,10 @@
 
 #include <stdint.h>
 
-#define LICENSE_KEY_TYPE        "tobedone"
-#define LICENSE_KEY_LIMIT_CONNS "tobedone"
+#define LICENSE_KEY_LIMIT_CONNS  "1.3.6.1.4.1.52509.1"
+#define LICENSE_KEY_PLUGINS      "1.3.6.1.4.1.52509.2" // unused
+#define LICENSE_KEY_TYPE         "1.3.6.1.4.1.52509.3"
+#define LICENSE_KEY_COMPANY_TYPE "1.3.6.1.4.1.52509.4" // unused
 
 int lic_std_init(const char *path);
 int lic_std_update(uint32_t addon);
