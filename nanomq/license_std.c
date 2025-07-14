ce77938540a84ff5457ff2383a9b007a1a94075d
wanghaemq <wangwei@emqx.io>
2025-07-14 03:54:05 -0400

    * NEW [license] Support to base64 decode license.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

--- nanomq/license_std.c
+++ nanomq/license_std.c
@@ -17,6 +17,39 @@
 #include <openssl/x509.h>
 #include <openssl/x509_vfy.h>
 
+// from kernel
+static int
+base64_decode(const char *src, int srclen, uint8_t *dst)
+{
+	uint32_t ac = 0;
+	int bits = 0;
+	int i;
+	uint8_t *bp = dst;
+
+	for (i = 0; i < srclen; i++) {
+		const char *p = strchr(base64_table, src[i]);
+
+		if (src[i] == '=') {
+			ac = (ac << 6);
+			bits += 6;
+			if (bits >= 8)
+				bits -= 8;
+			continue;
+		}
+		if (p == NULL || src[i] == 0)
+			return -1;
+		ac = (ac << 6) | (p - base64_table);
+		bits += 6;
+		if (bits >= 8) {
+			bits -= 8;
+			*bp++ = (uint8_t)(ac >> bits);
+		}
+	}
+	if (ac & ((1 << bits) - 1))
+		return -1;
+	return bp - dst;
+}
+
 static char *
 readfile(const char *fname, int *sz)
 {
@@ -76,34 +109,33 @@ static int
 parse_lic_str(const char *data, const char *pubk, lic_std *lic)
 {
 	int   rv      = 0;
-	BIO  *certbio = NULL;
-	X509 *cert    = NULL;
 
-	const char * lic_args = NULL;
-	const char * lic_sign = NULL;
-	if (0 != (rv = split_lic_str(data, &lic_args, &lic_sign))) {
+	const char * lic_args_b64 = NULL;
+	const char * lic_sign_b64 = NULL;
+	if (0 != (rv = split_lic_str(data, &lic_args_b64, &lic_sign_b64))) {
 		log_warn("invalid lic format");
 		return rv;
 	}
 
-	if (NULL == (certbio = BIO_new(BIO_s_mem()))) {
+	const char * lic_args = nng_alloc(sizeof(char) * strlen(lic_args_b64));
+	const char * lic_sign = nng_alloc(sizeof(char) * strlen(lic_sign_b64));
+	const int lic_args_sz;
+	const int lic_sign_sz;
+	if (!lic_args || !lic_sign) {
 		return NNG_ENOMEM;
 	}
-	if (0 >= BIO_puts(certbio, data)) {
-		BIO_free_all(certbio);
+
+	if ((lic_args_sz = base64_decode(lic_args, strlen(lic_args_b64), lic_args)) <= 0) {
+		log_warn("invalid lic content");
 		return NNG_EINVAL;
 	}
-	if (!(cert = PEM_read_bio_X509(certbio, NULL, NULL, NULL))) {
-		log_warn("failed to load certificate data into memory");
-		BIO_free_all(certbio);
+	if ((lic_sign_sz = base64_decode(lic_sign, strlen(lic_sign_b64), lic_sign)) <= 0) {
+		log_warn("invalid lic content");
 		return NNG_EINVAL;
 	}
+	log_info("args: [%s] sign: [%.*s]", lic_args, lic_sign_sz, lic_sign);
 
-	rv = parse_lic(cert, pubk, lic);
-
-	X509_free(cert);
-	BIO_free_all(certbio);
-	return rv;
+	return lic_verify(lic, lic_sign_sz, lic_sign, pubk);
 }
 
 static int
