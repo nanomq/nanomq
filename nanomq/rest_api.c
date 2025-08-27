bd3ecc7b265ae44c5a7f8d087c9384f89f85d169
wanghaemq <wangwei@emqx.io>
2025-08-27 05:47:56 -0400

    * FIX [restapi] Fix the error that failed to get params when one param contained in get request url.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

--- nanomq/rest_api.c
+++ nanomq/rest_api.c
@@ -569,7 +569,7 @@ uri_param_parse(const char *path, size_t *count)
 		memcpy(kv_str[num - 1], str, len - 1);
 		str = ret + 1;
 	}
-	if (num > 0) {
+	if (num > 0 || strchr(str, '=')) {
 		num++;
 		char **new_kv_str;
 		new_kv_str = realloc(kv_str, sizeof(char *) * num);
