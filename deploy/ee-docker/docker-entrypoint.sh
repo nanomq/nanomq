0868bac39fff6ca2bd80f18b0425641ccdc4a1fe
wanghaemq <wangwei@emqx.io>
2025-11-05 04:59:32 -0500

    * NEW [deploy] New dockerfile to start emqx-edge.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

---
+++ deploy/ee-docker/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -n "$DEBUG" ]; then
+    set -ex
+else
+    set -e
+fi
+
+_main() {
+	if [ -f "/tmp/nanomq/nanomq.pid" ];then
+		rm -f /tmp/nanomq/nanomq.pid
+	fi
+
+	if [ "$#" -eq 0 ];then
+		set -- ./nanomq start
+	elif [ "${1#-}" != "$1" ]; then
+		set -- ./nanomq start "$@"
+	fi
+
+	exec "$@"
+}
+
+_main "$@"
