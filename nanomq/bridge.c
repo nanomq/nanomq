f578ca5daf05ca93b6399266d9bcf6dd879e8f3b
Jaylin <jaylin@emqx.io>
2026-01-14 11:52:38 +0800

    * MDF [bridge] pass conf_sqlite to bridge_conf sqlite
    Signed-off-by: Jaylin <jaylin@emqx.io>

--- nanomq/bridge.c
+++ nanomq/bridge.c
@@ -62,7 +62,7 @@ apply_sqlite_config(
 		NANO_NNG_FATAL("Initializing SQLite with nng_mqtt_alloc_sqlite_opt", rv);
 	}
 
-	nng_mqtt_set_sqlite_conf(opt, config, config->name);
+	nng_mqtt_set_sqlite_conf(opt, config, config->name, config->proto_ver, config->sqlite);
 	// init sqlite db
 	nng_mqtt_sqlite_db_init(opt, db_name);
 
