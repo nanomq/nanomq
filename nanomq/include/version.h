40287884f0159813ebf26929ffed22cf840c2857
Jaylin <jaylin@emqx.io>
2026-01-19 18:48:45 +0800

    move nng head & ver num for new release
    Signed-off-by: Jaylin <jaylin@emqx.io>

--- nanomq/include/version.h
+++ nanomq/include/version.h
@@ -1,4 +1,4 @@
 #define NANO_VER_MAJOR 1
-#define NANO_VER_MINOR 2
-#define NANO_VER_PATCH 1
-#define NANO_VER_ID_SHORT "16"
+#define NANO_VER_MINOR 3
+#define NANO_VER_PATCH 2
+#define NANO_VER_ID_SHORT "17"
