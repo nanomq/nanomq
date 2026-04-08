d71409fafae55ef682a1e94e88bdccfed724ca87
JaylinYu <letrangerjaylin@gmail.com>
2026-04-08 18:29:48 +0800

    * FIX [pub_handler] #2270
    Signed-off-by: JaylinYu <letrangerjaylin@gmail.com>

--- nanomq/pub_handler.c
+++ nanomq/pub_handler.c
@@ -2123,6 +2123,11 @@ decode_pub_message(nano_work *work, uint8_t proto)
 		    pub_packet->fixed_header.qos);
 
 		if (pub_packet->fixed_header.qos > 0) {
+			if (pos + 2 > msg_len) {
+				log_error("Invalid msg: Protocol error! Missing packet identifier for QoS %d.", 
+				          pub_packet->fixed_header.qos);
+				return PROTOCOL_ERROR;
+			}
 			NNI_GET16(msg_body + pos,
 			    pub_packet->var_header.publish.packet_id);
 			log_debug("identifier: [%d]",
