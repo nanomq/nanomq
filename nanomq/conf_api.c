c963106f8577baf8503e725b6a68d893258f86fe
JaylinYu <letrangerjaylin@gmail.com>
2026-08-12 15:33:29 +0800

    * FIX [conf_api] alloc new config struct and assign it
    Signed-off-by: JaylinYu <letrangerjaylin@gmail.com>

--- nanomq/conf_api.c
+++ nanomq/conf_api.c
@@ -855,9 +855,11 @@ set_auth_http_req(cJSON *json, const char *conf_path, conf_auth_http_req *req,
 			// conf_update2(
 			//     conf_path, key_prefix, "headers.", key, value);
 			if (header_count <= index) {
+				conf_http_header *hdr = nng_zalloc(sizeof(conf_http_header));
 				req->header_count++;
-				req->headers = realloc(req->headers, (req->header_count) * sizeof(conf_http_header *));
-				req->headers[index] = nng_zalloc(sizeof(conf_http_header));
+				req->headers = realloc(req->headers,
+				    (req->header_count) * sizeof(char *));
+				req->headers[index] = hdr;
 			}
 			update_string(req->headers[index]->key, key);
 			update_string(req->headers[index]->value, value);
