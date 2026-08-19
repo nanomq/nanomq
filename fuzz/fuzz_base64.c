b1e84a4c5cb9fa8ed1b6be7bdc6c1c9293215078
JaylinYu <letrangerjaylin@gmail.com>
2026-08-19 12:14:13 +0800

    * FIX [fuzz_base64] fix wrong base64 api, updated to new one.
    Signed-off-by: JaylinYu <letrangerjaylin@gmail.com>

--- fuzz/fuzz_base64.c
+++ fuzz/fuzz_base64.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
-#include "nng/supplemental/nanolib/base64.h"
+#include "nng/supplemental/nanolib/nmq_base64.h"
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     char *enc_out = NULL;
     unsigned char *dec_out = NULL;
@@ -10,14 +10,14 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         unsigned int enc_sz = BASE64_ENCODE_OUT_SIZE(size);
         enc_out = (char *)malloc(enc_sz);
         if (enc_out) {
-            base64_encode(data, (unsigned int)size, enc_out);
+            nmq_base64_encode(data, (unsigned int)size, enc_out);
         }
     }
     if (size > 0) {
         unsigned int dec_sz = BASE64_DECODE_OUT_SIZE(size);
         dec_out = (unsigned char *)malloc(dec_sz);
         if (dec_out) {
-            base64_decode((const char *)data, (unsigned int)size, dec_out);
+            nmq_base64_decode((const char *)data, (unsigned int)size, dec_out);
         }
     }
     free(enc_out);
