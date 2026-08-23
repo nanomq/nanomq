de3a224ff57788a2f30791c304c2cdf9cd519baf
Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>
2026-08-23 11:10:38 +0200

    fix: align base64 fuzz harness with nng API
    Signed-off-by: Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>

--- fuzz/fuzz_base64.c
+++ fuzz/fuzz_base64.c
@@ -7,17 +7,17 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     char *enc_out = NULL;
     unsigned char *dec_out = NULL;
     if (size > 0) {
-        unsigned int enc_sz = BASE64_ENCODE_OUT_SIZE(size);
+        size_t enc_sz = BASE64_ENCODE_OUT_SIZE(size);
         enc_out = (char *)malloc(enc_sz);
         if (enc_out) {
-            nmq_base64_encode(data, (unsigned int)size, enc_out);
+            nmq_base64_encode(data, size, enc_out, enc_sz);
         }
     }
     if (size > 0) {
-        unsigned int dec_sz = BASE64_DECODE_OUT_SIZE(size);
+        size_t dec_sz = BASE64_DECODE_OUT_SIZE(size);
         dec_out = (unsigned char *)malloc(dec_sz);
         if (dec_out) {
-            nmq_base64_decode((const char *)data, (unsigned int)size, dec_out);
+            nmq_base64_decode((const char *)data, size, dec_out, dec_sz);
         }
     }
     free(enc_out);
